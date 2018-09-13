88818725e532b1d9c6b6c43d24343ed504c81d12
Andrei Fitai <fitai.andrei@gmail.com>
2018-09-13 10:49:59 +0200

    initial setup

---
+++ src/js/Application.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Application extends React.Component {
+    render() {
+        return (
+            <div className="application">
+                <h1>Our first React Component, which is in another file!</h1>
+            </div>
+        )
+    }
+}
+
+export default Application
