90dc43cb946a66ea99ead4a8e5c933b60e9068bb
Irving Andrade <irving.an.ve@gmail.com>
2018-10-03 15:02:19 -0500

    casi terminado

---
+++ thinkingonyouwhyyouleftme/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FindContainer from './components/FindContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <FindContainer/>
+      </div>
+    );
+  }
+}
+
+export default App;
