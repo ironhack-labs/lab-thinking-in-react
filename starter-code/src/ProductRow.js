c5a6ac32574d6c6506a89d889a50e32f9a604604
Travis Grubbs <flygrubbs@yahoo.com>
2018-12-17 09:15:11 -0500

    cleaned code

--- starter-code/src/ProductRow.js
+++ starter-code/src/ProductRow.js
@@ -1,30 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
 import products from './data.json';
 
-
-
 class ProductRow extends Component {
-
-
-    render(){
-
-return(
-
-    <div>
-     this is {products.category}
-    </div>
-)
-
-    }
-
-
+	render() {
+		return <div>this is {products.category}</div>;
+	}
 }
-
-
-
-
-
 
 export default ProductRow;
--- starter-code/src/ProductTable.js
+++ starter-code/src/ProductTable.js
@@ -1,36 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
 import ProductCategoryRow from './ProductCategoryRow';
 import ProductRow from './ProductRow';
 
-
 class ProductTable extends Component {
-
-
-    render(){
-
-
-        return(
-
-            <div className="columns">
-             
-                <ProductCategoryRow />
-
-            </div>
-
-        )
-    }
-
-
+	render() {
+		return (
+			<div className="columns">
+				<ProductCategoryRow />
+			</div>
+		);
+	}
 }
 
-
-
 //<productCategoryrow />
-
-
-
-
 
 export default ProductTable;
