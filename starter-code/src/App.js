9a048249dc88d81d52b23155773dccd8e9ac974a
Teresa Romero <tromerolozano@gmail.com>
2019-01-16 12:18:27 +0100

    iteration 3 filter products

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import data from "./data.json";
-import { SearchBar } from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
 import { ProductTable } from "./components/ProductTable";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      productData: data.data
+      productData: data.data,
+      filterData : data.data
     };
+  }
+  // en el ejercicio anterior use las funciones 
+  // componentDidMount() y componentWillReceiveProps(nextProps)
+  // para asignarle a filterData el valor inicial y que cargue la lista en el primer render
+  // la unica manera que se me ha ocurrido es pasarle como estado original tambien data
+  // este estado es el que cambia la funcion handleFilter cada vez que buscamos en la barra de busqueda
+
+  handleFilter(filtered){
+    this.setState({filterData : filtered})
   }
 
   render() {
@@ -17,8 +27,8 @@ export default class App extends Component {
       <div className="App">
         <section className="section">
           <div className="container" id="FilterableProductTable">
-            <SearchBar />
-            <ProductTable prodData={this.state.productData} />
+            <SearchBar filtered={(filtered)=>this.handleFilter(filtered)} prodData={this.state.productData}/>
+            <ProductTable prodData={this.state.filterData} />
           </div>
         </section>
       </div>
--- starter-code/src/components/SearchBar.js
+++ starter-code/src/components/SearchBar.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const SearchBar = () => {
-  return (
-    <section className="section">
-      <div className="container" id="SearchBar">
-        <input className="input" type="text" placeholder="Search products..." />
+export default class SearchBar extends Component {
+  handleChange(e) {
+    let initialList = [];
+    let newList = [];
 
-        <label className="checkbox">
-          <input type="checkbox" />
-          Only show products in stock
-        </label>
-      </div>
-    </section>
-  );
-};
+    if (e.target.value !== "") {
+      initialList = this.props.prodData;
+      newList = initialList.filter(prod => {
+        const lc = prod.name.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
+      });
+    } else {
+      newList = this.props.prodData;
+    }
+
+    this.props.filtered(newList);
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container" id="SearchBar">
+          <input
+            className="input"
+            type="text"
+            placeholder="Search products..."
+            onChange={e => {
+              this.handleChange(e);
+            }}
+          />
+
+          <label className="checkbox">
+            <input type="checkbox" />
+            Only show products in stock
+          </label>
+        </div>
+      </section>
+    );
+  }
+}
