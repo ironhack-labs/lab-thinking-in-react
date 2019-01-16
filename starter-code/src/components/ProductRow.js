80ed335116e7ebab632c9fc4a7258dae98c706e9
Teresa Romero <tromerolozano@gmail.com>
2019-01-16 20:15:22 +0100

    red color for non stocked and final styling

--- starter-code/src/components/ProductRow.js
+++ starter-code/src/components/ProductRow.js
@@ -1,17 +1,36 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+export const StyledRow = styled.tr`
+  &.stocked {
+    color: red;
+  }
+`;
 
 export const ProductRow = ({ rowCat, prodData }) => {
   return (
     <React.Fragment>
       {prodData
         .filter(prod => prod.category === rowCat)
-        .map(prod => (
-          <tr key={prod.name}>
-            <td>{prod.name}</td>
-            <td>{prod.price}</td>
-            {prod.stocked ? <td><i className="fas fa-check-circle"></i></td> : <td><i className="fas fa-exclamation-circle"></i></td>}
-          </tr>
-        ))}
+        .map(prod =>
+          prod.stocked ? (
+            <StyledRow key={prod.name}>
+              <td>{prod.name}</td>
+              <td>{prod.price}</td>
+              <td>
+                <i className="fas fa-check-circle" />
+              </td>
+            </StyledRow>
+          ) : (
+            <StyledRow key={prod.name} className="stocked">
+              <td>{prod.name}</td>
+              <td>{prod.price}</td>
+              <td>
+                <i className="fas fa-exclamation-circle" />
+              </td>
+            </StyledRow>
+          )
+        )}
     </React.Fragment>
   );
 };
