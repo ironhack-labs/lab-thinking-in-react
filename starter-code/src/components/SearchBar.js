5a6a8559c6f21f1fb0c55aefa5b5cb8d4595f278
Teresa Romero <tromerolozano@gmail.com>
2019-01-16 19:48:29 +0100

    checkbox implemented with searchbar

--- starter-code/src/components/SearchBar.js
+++ starter-code/src/components/SearchBar.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      filteredList : [],
-      checkBox : true
-    }
+    this.state = {
+      filteredList: [],
+      checkBox: true
+    };
   }
-  
+
   handleSearchBar(e) {
     let initialList = [];
     let newList = [];
@@ -21,15 +20,20 @@ export default class SearchBar extends Component {
         const filter = e.target.value.toLowerCase();
         return lc.includes(filter);
       });
+      this.setState({ filteredList: newList });
     } else {
       newList = this.props.prodData;
+      this.setState({ filteredList: newList });
     }
-
     this.props.filtered(newList);
   }
 
-  handleCheckbox(){
-    this.setState({checkBox : !this.state.checkBox})
+  handleCheckbox() {
+    this.setState({ checkBox: !this.state.checkBox });
+    let stockProd = this.state.filteredList.filter(prod => prod.stocked);
+    this.state.checkBox
+      ? this.props.filtered(stockProd)
+      : this.props.filtered(this.state.filteredList);
   }
 
   render() {
@@ -46,7 +50,12 @@ export default class SearchBar extends Component {
           />
 
           <label className="checkbox">
-            <input type="checkbox" onClick={()=>{this.handleCheckbox()}}/>
+            <input
+              type="checkbox"
+              onClick={() => {
+                this.handleCheckbox();
+              }}
+            />
             Only show products in stock
           </label>
         </div>
