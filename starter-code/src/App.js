c99d4268f07e56ff829053b54325f34caa9bc863
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-02-21 16:45:32 +0100

    FilterableProductTable file included

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from './data';
-import Products from './Components/products';
-import SearchBar from './Components/searchBar';
-
-console.log(data.data)
+import FilterableProductTable from './Components/FilterableProductTable';
 
 class App extends Component {
-
-  state = {
-    data: data.data
-  }
-
-  searchFunction = (searchWord, checked) => {
-    let newState = {
-      ...this.state
-    }
-    if (checked) {
-      newState.data = data.data.filter(product => {
-        return (product.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1 && product.stocked);
-      })
-    }
-
-    else {
-    newState.data = data.data.filter(product => {
-      return product.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1;
-    })
-    }
-
-    this.setState(newState)
-  }
 
   render() {
     return (
       <div className="App">
-        <SearchBar searchFunctionProp={this.searchFunction} />
-        <Products dataProp={this.state.data} />
+        <FilterableProductTable />
       </div>
     );
   }
---
+++ starter-code/src/Components/FilterableProductTable.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Products from './products';
+import SearchBar from './searchBar';
+import data from '../data';
+
+export default class FilterableProductTable extends Component {
+
+  state = {
+    data: data.data
+  }
+
+  searchFunction = (searchWord, checked) => {
+    let newState = {
+      ...this.state
+    }
+    if (checked) {
+      newState.data = data.data.filter(product => {
+        return (product.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1 && product.stocked);
+      })
+    }
+
+    else {
+      newState.data = data.data.filter(product => {
+        return product.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1;
+      })
+    }
+
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar searchFunctionProp={this.searchFunction} />
+        <Products dataProp={this.state.data} />
+      </div>
+    )
+  }
+}
