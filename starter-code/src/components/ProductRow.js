9da875d8ef8e4063ea2992ed65a6780db8d45dc8
André Sebastian <a.sebastian@posteo.de>
2019-02-24 14:14:22 +0100

    add red color, if item is not stocked

--- starter-code/src/components/ProductRow.js
+++ starter-code/src/components/ProductRow.js
@@ -4,10 +4,13 @@ import React, { Component } from 'react';
 class ProductRow extends Component {
 
   render() {
+    let styleNotStocked = {
+      color: "red"
+    }
     return (
       <tr className="ProductRow">
           <td>{this.props.product.name}</td>
-          <td>{this.props.product.stocked ? "Yes" : "No"}</td>
+          <td>{this.props.product.stocked ? "Yes" : <span style={styleNotStocked}>No</span>}</td>
           <td>{this.props.product.price}</td>
       </tr>
     );
