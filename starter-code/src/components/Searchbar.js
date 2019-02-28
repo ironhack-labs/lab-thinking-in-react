8c9fe62c5f79ca271e656b6e779b34225a3c7630
Sarah Dröse <sarahdroese@Sarah.local>
2019-02-28 17:12:38 +0100

    Done

--- starter-code/src/components/Searchbar.js
+++ starter-code/src/components/Searchbar.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 
-class Searchbar extends Component {
-
-    onChange = (event) => {
-        const value = event.target.value
-        this.props.onSearch(value)
-    }
-
-    render() {
-
-        this.props.blub // => 'bla'
-
-        return (
-            <div>
-
-                <input onChange={this.onChange} type="text" placeholder="search" />
-
-            </div>
-
+const searchBar = (props)=>{
+    return (
+        <input type="text" onChange={(e) => props.onSearch(e.target.value)}>
+    {props.searchTerm}
+    </input>
+    
         )
-    }
 }
 
-export default Searchbar
+
+// class Searchbar extends Component {
+
+//     onChange = (event) => {
+//         const value = event.target.value
+//         this.props.onSearch(value)
+//     }
+
+//     render() {
+
+//         this.props.blub // => 'bla'
+
+//         return (
+//             <div>
+
+//                 <input onChange={this.onChange} type="text" placeholder="search" />
+
+//             </div>
+
+//         )
+//     }
+// }
+
+export default searchBar
