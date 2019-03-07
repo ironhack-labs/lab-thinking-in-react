025c27b490a2a8ee8059f5deb7659f33e33bffb7
Jack Fischer <jrfisch95@gmail.com>
2019-03-07 11:50:39 -0500

    finished bonus question

--- starter-code/src/components/SearchBar.js
+++ starter-code/src/components/SearchBar.js
@@ -9,33 +9,44 @@ class SearchBar extends Component{
     }
   }
 
-  
-
-  handleSearchText = (e) => {
+  doSearching = () =>{
     if(document.getElementById('instock').checked){
       const stockedProducts = this.props.products.filter(product => product.stocked);
-      this.state.filterUpdate(stockedProducts.filter((product) => {return product.name.toUpperCase().includes(e.target.value.toUpperCase()); }));
+      this.state.filterUpdate(stockedProducts.filter((product) => {return product.name.toUpperCase().includes(document.getElementById('search').value.toUpperCase()); }));
     } else{
-      this.state.filterUpdate(this.props.products.filter((product) => {return product.name.toUpperCase().includes(e.target.value.toUpperCase()); }));
-    }
+      this.state.filterUpdate(this.props.products.filter((product) => {return product.name.toUpperCase().includes(document.getElementById('search').value.toUpperCase()); }));
+    } 
   }
 
-  handleSearchCheckBox = (e) => {
-    if(document.getElementById('instock').checked){
-      this.state.filterUpdate(this.props.products.filter(product => product.stocked));
-    } else{
-      this.state.filterUpdate(this.props.products);
-    }
+  // handleSearchText = (e) => {
+  //   if(document.getElementById('instock').checked){
+  //     const stockedProducts = this.props.products.filter(product => product.stocked);
+  //     this.state.filterUpdate(stockedProducts.filter((product) => {return product.name.toUpperCase().includes(e.target.value.toUpperCase()); }));
+  //   } else{
+  //     this.state.filterUpdate(this.props.products.filter((product) => {return product.name.toUpperCase().includes(e.target.value.toUpperCase()); }));
+  //   }
+  // }
+
+  // handleSearchCheckBox = (e) => {
+  //   if(document.getElementById('instock').checked){
+  //     this.state.filterUpdate(this.props.products.filter(product => product.stocked));
+  //   } else{
+  //     this.state.filterUpdate(this.props.products);
+  //   }
     
-  }
+  // }
+
+  // handleChange = () => {
+  //   this.doSearching();
+  // }
 
   render(){
     return (
       <div>
         <h3>Search</h3>
-        <input type='text' onChange={(e) => this.handleSearchText(e)} id='search'></input>
+        <input type='text' onChange={this.doSearching} id='search'></input>
         <br></br>
-        <input type='checkbox' onChange={(e) => this.handleSearchCheckBox(e)} id='instock'></input>
+        <input type='checkbox' onChange={this.doSearching} id='instock'></input>
         <label htmlFor='instock'>Only show products in stock</label>
       </div>
       
