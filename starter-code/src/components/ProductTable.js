941bbeeef62de5d48460d8bac754b0ea546479c0
Monroe <mgetz10@gmail.com>
2019-03-07 13:00:36 -0500

    passing data down

--- starter-code/src/components/ProductTable.js
+++ starter-code/src/components/ProductTable.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class productTable extends Component {
   render() {
-    return <div className="productTableContainer" />;
+    return (
+      <div className="productTableContainer">
+        <div className="flex tableHead row">
+          <h4>Name</h4>
+          <h4>Price</h4>
+        </div>
+      </div>
+    );
   }
 }
 
