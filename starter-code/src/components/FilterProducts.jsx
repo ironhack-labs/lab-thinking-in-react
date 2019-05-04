b088607ce1626b6dd5f28d69b915a7588a2227e1
alex fox <to.alexfox@gmail.com>
2019-05-04 20:52:19 +0200

    search working but not remove character

--- starter-code/src/components/FilterProducts.jsx
+++ starter-code/src/components/FilterProducts.jsx
@@ -6,20 +6,44 @@ export default class FilteredProducts extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			"filteredProducts" : props
+			"products" : props.data
 		}
 	}
 
+	objectContainsString(string, object) {
+		var compString = string.toLowerCase()
+
+		for (var key in object) {
+			if (typeof(object[key]) !== "string" ) console.log("not a string.")
+			else {
+				var compObjectValue = object[key].toLowerCase()
+				if (compObjectValue.includes(compString)) return true;
+		}}
+
+		return false;
+	}
+
 	searchFilter(searchTerm) {
-		console.log("here i am, and here is the search term: ", searchTerm)
+		var filteredProducts = this.state.products.filter( product => 
+			this.objectContainsString(searchTerm, product)
+		)
+
+		console.log("new filtered: ", filteredProducts);
+		this.setState({"products" : filteredProducts},
+			console.log("new state: ", this.state)
+		)
 	}
 
 	render() {
+		// const object = {"category": "Sporting Goods", "name": "Football"}
+		// var test = this.objectContainsString("sport", object)
+		// console.log("test: ", test)
+
 		return(
 		<div className="productsContainer">
 			<h1 className="heading">IronStore</h1>
 			<SearchBar updateTable={(searchTerm) => this.searchFilter(searchTerm)} />
-			<ProductTable {...this.state.filteredProducts} />
+			<ProductTable products={this.state.products} />
 		</div>
 	)
 	}
--- starter-code/src/components/ProductTable.jsx
+++ starter-code/src/components/ProductTable.jsx
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import ProductRow from "./ProductRow"
 
-export default class ProductTable extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			"productList" : this.props.data
-		}
-	}
+export default function productTable(props) {
+	var products = props.products
+	console.log(products)
 
-	render() {
-		return(
-			<table className="productTable">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Category</th>
-						<th>Price</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						this.state.productList.map( (product, index) => (
-							<ProductRow key={index} {...product} />
-						))
-					}
-				</tbody>
-			</table>
-		)
-	}
+	return(
+		<table className="productTable">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Category</th>
+					<th>Price</th>
+				</tr>
+			</thead>
+			<tbody>
+				{
+					props.products.map( (product, index) => (
+						<ProductRow key={index} {...product} />
+					))
+				}
+			</tbody>
+		</table>
+	)
 }
