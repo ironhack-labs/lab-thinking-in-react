11ad78334d0b4584140e20952ff8cb6bd411b08b
Jairo Garcia <jgarcia@indratek.com>
2019-05-14 13:55:58 -0500

    Iteration 1

---
+++ starter-code/src/components/FilterableProductTable.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FilterableProductTable extends Component {
+  render() {
+    return <div>Productos Filtrados</div>;
+  }
+}
+
+export default FilterableProductTable;
