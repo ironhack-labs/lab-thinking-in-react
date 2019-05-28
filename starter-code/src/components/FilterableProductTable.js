b088fcc2e8547b8f4fa6e3241a2be4d2c9e119ad
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-05-28 22:50:46 +0200

    it 4 con errores al filtrar por txt

--- starter-code/src/components/FilterableProductTable.js
+++ starter-code/src/components/FilterableProductTable.js
@@ -5,15 +5,44 @@ import ProductTable from './ProductTable';
 class FilterableProductTable extends React.Component {
   state = {
     products: this.props.data,
-    text:''
+    text:'',
   }
 
-  filterBy = (someState) => {
+  filterBy = (event) => {
     this.setState({
-      products: this.props.data.filter(item => item.name.toLowerCase().includes(someState.toLowerCase())),
-      text:someState
+      products: this.props.data.filter(product => product.name.toLowerCase().includes(event.toLowerCase())),
+      text:event
     })
   }
+
+  checkBox = (e) => {
+    if (e === false) {
+      this.setState({
+        products: this.props.data.filter(product => product.stocked === true),
+        text: this.state.text
+      }) 
+    } else {
+      this.setState({
+        products: this.props.data,
+        text: this.state.text
+      }) 
+    }
+  }
+
+  render(){
+    return (  
+      <div>
+        <h1>IronStore</h1>
+        <SearchBar inStock={this.checkBox} stockSate={this.state.inStock} filterBy = {this.filterBy} text={this.state.text}/>
+        <ProductTable products={this.state.products}/>
+      </div>
+    )
+  }
+}
+
+export default FilterableProductTable
+
+
   /* por qué cojones este no va y el otro si?¿
     filterBy = (someState) => {
     const filteredProducts = this.props.data.filter(element => element.name.toUpperCase().includes(this.state.text.toUpperCase()))
@@ -23,19 +52,3 @@ class FilterableProductTable extends React.Component {
     })
   }
   */
-
-  render(){
-    console.log(this.state.text)
-    console.log(this.state.products)
-
-    return (  
-      <div>
-        <h1>IronStore</h1>
-        <SearchBar filterBy = {this.filterBy} text={this.state.text}/>
-        <ProductTable products={this.state.products}/>
-      </div>
-    )
-  }
-}
-
-export default FilterableProductTable
--- starter-code/src/components/SearchBar.js
+++ starter-code/src/components/SearchBar.js
@@ -1,19 +1,37 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
-
+  state = {
+    checked:false
+  }
 
   handleChange = (event) => {
     this.props.filterBy(event.target.value)
   }
 
+  handleCheckBox = (event) => {
+    this.props.inStock(event.target)
+  }
+
+  handleOnOff = () => {
+    this.checkFunction(!this.state.checked)
+  }
+
+  checkFunction = (e) => {
+    this.setState({
+      checked: e
+    })
+    this.props.inStock(this.state.checked)
+  }
   render(){
+    console.log(this.state.checked)
+
     return (
       <form>
           <label>Search</label>
             <input value={this.props.text} onChange={(e) => this.handleChange(e)} className="form-control" type="text" aria-label="Search" />
           <label>Only show products on stock</label>
-            <input type="checkbox"/>
+            <input type="checkbox" name="inStock" checked={this.state.checked} onChange={this.handleOnOff}/>
       </form>
     )
   }
