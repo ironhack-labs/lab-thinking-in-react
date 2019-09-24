9afc34635b976e0262530a9b67fb705733c2d89a
José Carneiro <josemcarneiro@gmail.com>
2019-09-24 15:12:12 +0100

    Updated dependencies, removed package-lock, ignored yarn.lock and changed js files to jsx

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -17,5 +17,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
