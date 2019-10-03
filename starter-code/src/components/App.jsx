f4a64b869fd87ba70ff07b51828f0e7ad8668226
Antonio Llamas <antoniollamasln@gmail.com>
2019-10-04 00:21:11 +0200

    Add iteration 1

---
+++ starter-code/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import FilterableProductTable from './pages/FilterableProductTable/FilterableProductTable'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <FilterableProductTable /> 
+      </div>
+    )
+  }
+}
