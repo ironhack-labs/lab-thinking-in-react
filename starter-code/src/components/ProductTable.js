44ce4f0520dff732e7d8730e8ab04b82898e7187
Evelyn Graetz <evelyn.graetz@gmx.de>
2019-10-31 21:26:57 +0100

    create all components

---
+++ starter-code/src/components/ProductTable.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ProductTable extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ starter-code/src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
