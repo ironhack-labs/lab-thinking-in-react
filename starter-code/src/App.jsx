0e340ecef83dc653682cb1187e62230a81758c9d
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-11-13 17:39:32 -0500

    mostly done

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,18 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import theData from './data.json'
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      list: theData.data,
+      search: '',
+      toggled: false,
+    }
+  }
+
+  searchBar = (e) =>{
+    
+    this.setState({
+      [e.target.name]: e.target.value,
+    },()=>{
+      let tempList = [...theData.data]
+      let finalList = tempList.filter(each => each.name.toLowerCase().includes(this.state.search.toLowerCase()))
+      this.setState({
+        list: finalList
+      })
+    })
+  }
+
+  updateInput = (e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    }, ()=>{
+      console.log(this.state)
+    })
+  }
+
+  decideRed = (e) =>{
+    console.log('ok')
+    if(e.target.stocked){
+      e.target.className = "red"
+    }else{
+      e.target.className = "blue"
+    }
+  }
+
+  toggleStocked = () =>{
+    this.setState({
+      toggled: !this.state.toggled
+    })
+  }
+
+  allItems = () =>{
+    return this.state.list.map((eachItem, i)=>{
+      let red = "";
+      if(!eachItem.stocked){
+        red = "red"
+      }
+      if(this.state.toggled){
+        if(red === ""){return (
+          <div className="box" key={i}>
+            <strong>{eachItem.category}</strong>
+            <p>
+                <span className={red}> {eachItem.name} </span>
+                <small>{eachItem.price}</small>
+            </p>
+          </div>
+        )}
+      }else{
+        return (
+          <div className="box" key={i}>
+            <strong>{eachItem.category}</strong>
+            <p>
+                <span className={red}> {eachItem.name} </span>
+                <small>{eachItem.price}</small>
+            </p>
+          </div>
+        )
+      }
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      Stocked Only: <input type="checkbox" onClick={this.toggleStocked}></input>
+      <input type="text" value={this.state.search} className="searchFood" 
+      name="search" onChange = {this.searchBar} placeholder="Search..." />
+      <p>
+        <strong>Name Price</strong>
+      </p>
+      {this.allItems()}
       </div>
     );
   }
