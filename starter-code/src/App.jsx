583db97ed84a812058115c34413a399c5ded6078
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-15 14:42:59 -0500

    add h1 to App.js

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import FilterableProductTable from './components/FilterableProductTable';
-import {data} from './data.json';
+import { data } from './data.json';
 
 export default class App extends Component {
-  state={
-    products:data,
-  }
+  state = {
+    products: data
+  };
   render() {
     return (
       <div className="App">
         <div>
-          <FilterableProductTable products={data} />
+          <h1>IronStore</h1>
         </div>
+        <FilterableProductTable products={data} />
       </div>
     );
   }
