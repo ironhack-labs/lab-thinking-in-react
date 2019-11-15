02ecb766d9ef7dc9ceb155dd13a633d8a16dafe8
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-15 14:50:07 -0500

    add serchbar and checkbox

--- starter-code/src/components/FilterableProductTable.jsx
+++ starter-code/src/components/FilterableProductTable.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 
-export default class 
- extends Component {
-    render() {
-        return (
-            <div>
-                <ProductTable products={this.props.products}/>
-            </div>
-        )
-    }
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
 }
--- starter-code/src/components/SearchBar.jsx
+++ starter-code/src/components/SearchBar.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="">Search</label>
+          <input type="text" name="search" id="search" />
+        </form>
+        <form action="">
+          <input type="checkbox" />
+          <label htmlFor="checkbox">Only show products in stock</label>
+        </form>
+      </div>
+    );
+  }
 }
