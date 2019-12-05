caffe08036a8fa703bc4aebdda446bcfb345a0d8
María Simó <maria@coponstudio.es>
2019-12-05 16:34:41 +0100

    Added styled or something

--- starter-code/src/components/filterableProductTable/SearchBar.jsx
+++ starter-code/src/components/filterableProductTable/SearchBar.jsx
@@ -3,17 +3,27 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   render() {
     return (
-      <div className="search">
+      <div className="container search d-flex flex-column aling-center mb-4">
         <h2>Search</h2>
         <input
           type="text"
-          className="input search-bar"
+          className="input search-bar mb-3 p-2"
           placeholder="Search a product"
-          onChange={(e) => this.props.findProducts(e)}
+          onChange={e => this.props.findProducts(e)}
         />
 
-        <input type="checkbox" name="stocked" onChange={(e) => this.props.inStock(e)}/>
-        <label htmlFor="stocked">Only show products on stock</label>
+        <div className="form-check">
+          <input
+            type="checkbox"
+            name="stocked"
+            id="stocked"
+            className="form-check-input"
+            value=""
+            onChange={e => this.props.inStock(e)}
+          />
+          <label htmlFor="stocked" class="form-check-label">Only show products on stock</label>
+        </div>
+
       </div>
     );
   }
