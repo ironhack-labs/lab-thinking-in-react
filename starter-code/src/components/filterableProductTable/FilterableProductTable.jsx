499966f1f65251615193758dd30752f2a8714983
María Simó <maria@coponstudio.es>
2019-12-05 16:28:47 +0100

    It 4 bonus baby

--- starter-code/src/components/filterableProductTable/FilterableProductTable.jsx
+++ starter-code/src/components/filterableProductTable/FilterableProductTable.jsx
@@ -1,40 +1,57 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import ProductTable from './productTable/ProductTable';
-import data from '../../data.json'
-
+import data from '../../data.json';
 
 export default class FilterableProductTable extends React.Component {
-    constructor(){
-        super();
-        this.products = [...data.data]
-    
-        this.state = {
-          products : this.products
-        }
-      }
+  constructor() {
+    super();
+    this.products = [...data.data];
 
+    this.state = {
+      products: this.products,
+      stockChecked: false
+    };
+  }
 
-    findProducts(e){
-        let products = [...this.products]
-        let productsFiltered;
+  findProducts(e) {
+    let products = [...this.products];
+    let productsFiltered;
 
-        productsFiltered = products.filter(product => product.name.toLowerCase().includes(e.target.value.toLowerCase()))
-     
-        this.setState({
-            ...this.state,
-            products: productsFiltered
-        })
-    }
-    
-    render() {
-    console.log(this.state.products)
+    productsFiltered = products.filter(product =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
 
+    this.setState({
+      ...this.state,
+      products: productsFiltered
+    });
+  }
+
+  inStock(e) {
+    this.state.stockChecked = !this.state.stockChecked;
+    let products = [...this.products];
+    let productsInStock = products.filter(product => product.stocked === true);
+    let productsResult;
+
+    this.state.stockChecked ? productsResult = productsInStock : productsResult = products
+
+    this.setState({
+      ...this.state,
+      products: productsResult
+    });
+  }
+
+  render() {
     return (
       <React.Fragment>
         <h1>IronStore</h1>
 
-        <SearchBar products={this.state.products} findProducts={(e) => this.findProducts(e)}></SearchBar>
+        <SearchBar
+          products={this.state.products}
+          findProducts={e => this.findProducts(e)}
+          inStock={e => this.inStock(e)}
+        ></SearchBar>
         <ProductTable products={this.state.products}></ProductTable>
       </React.Fragment>
     );
--- starter-code/src/components/filterableProductTable/SearchBar.jsx
+++ starter-code/src/components/filterableProductTable/SearchBar.jsx
@@ -12,7 +12,7 @@ export default class SearchBar extends React.Component {
           onChange={(e) => this.props.findProducts(e)}
         />
 
-        <input type="checkbox" name="stocked" />
+        <input type="checkbox" name="stocked" onChange={(e) => this.props.inStock(e)}/>
         <label htmlFor="stocked">Only show products on stock</label>
       </div>
     );
