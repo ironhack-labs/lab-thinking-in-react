8e123ea54c49b4dae6dc3278bbbdf0d94c19144b
lpolon <lpolon@gmail.com>
2020-01-22 23:09:56 -0300

    iteration 3 done

--- starter-code/src/Components/FilterableProductTable.jsx
+++ starter-code/src/Components/FilterableProductTable.jsx
@@ -13,34 +13,46 @@ export default class FilterableProductTable extends Component {
       filteredData: data
     };
     this.handleChangeTextField = this.handleChangeTextField.bind(this);
-    // this.getFilteredData = this.getFilteredData.bind(this);
+    this.getFilteredData = this.getFilteredData.bind(this);
   }
 
   handleChangeTextField(event) {
     const { value, name } = event.target;
-    this.setState({
-      [name]: value
-    });
+    this.setState(
+      {
+        [name]: value
+      },
+      // TODO: Achei no google a opção de passar como callback. Fiquei curioso sobre quais são minhas outras opção. Que papai do chão me ilumine.
+      this.getFilteredData
+    );
+  }
+
+  getFilteredData() {
     const searchString = this.state.searchField;
     const data = this.state.data;
     let result;
     if (searchString.length === 0) {
       result = data;
     } else {
-      result = data.filter(e => e.includes(searchString));
+      result = data.filter(
+        e =>
+          e.name.toLowerCase().includes(searchString) ||
+          e.name.toUpperCase().includes(searchString)
+      );
     }
+    // nada está chamando essa função
+    console.log(result);
     this.setState({
       filteredData: result
     });
   }
-
-  // getFilteredData() {}
 
   render() {
     return (
       <div>
         <h1>Iron Store</h1>
         <SearchBar
+          // getFilteredData={this.getFilteredData}
           handleChangeTextField={this.handleChangeTextField}
           searchFieldValue={this.state.searchField}
         />
