89f554821dfb2091657d50eef93d39aa5ba07f94
Paul Dkn <pauldkn.code@gmail.com>
2020-02-22 17:14:57 +0100

    Checkbox ok

--- starter-code/src/components/FilterableProductTable.jsx
+++ starter-code/src/components/FilterableProductTable.jsx
@@ -10,52 +10,25 @@ export default class FilterableProductTable extends Component {
 
   filterHandler = input => {
     let filteredArticles = [...this.props.products];
-    if (input.type === "checkbox" && input.checked) {
-      this.setState({displayAll: false})
+    if (input.type === 'checkbox' && input.checked) {
+      this.setState({ displayAll: false });
       filteredArticles = [...filteredArticles.filter(a => a.stocked)];
     }
     console.log(filteredArticles);
     this.setState({
       articles: filteredArticles
-    })
+    });
 
-    if (input.type === "text") {
-      console.log(input.value)
-        filteredArticles.filter(a => {
-          a.name
-          .toLowerCase()
-          .includes(input.value.toLowerCase() || input.value)
-        })
-      }
-      this.setState({
-        articles: filteredArticles
-      })
+    if (input.type === 'text') {
+      console.log(input.value);
+      filteredArticles.filter(a => {
+        a.name.toLowerCase().includes(input.value.toLowerCase() || input.value);
+      });
     }
-
-    
-
-  // searchHandler = search => {
-  //   console.log(this.state.articles);
-  //   // this.checkHandler()
-
-  //   this.setState({
-  //     articles: this.props.products.filter(a =>
-  //       a.name.toLowerCase().includes(search.toLowerCase() || search)
-  //     )
-  //   });
-  // };
-
-  // checkHandler = () => {
-  //   if (this.state.articles === this.props.products) {
-  //     this.setState({
-  //       articles: this.props.products.filter(a => a.stocked)
-  //     });
-  //   } else {
-  //     this.setState({
-  //       articles: this.state.articles
-  //     });
-  //   }
-  // };
+    this.setState({
+      articles: filteredArticles
+    });
+  };
 
   render() {
     return (
