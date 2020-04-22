c3301897654bbce38cf30e177d4da13df64bee43
Michael Mueller <michael@miniguide.co>
2020-04-22 18:52:29 +0200

    done

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import data from './data.json';
 import Header from './components/Header';
 import FilterableProductTable from './components/FilterableProductTable';
+import data from './data.json';
 import './App.css';
 
 const App = () => {
