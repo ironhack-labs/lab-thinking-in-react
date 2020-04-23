274336532ed9157c1d7c8dda06515c6d79d1703f
Ben Balderas <benbalderas3@gmail.com>
2020-04-22 21:28:02 -0500

    Done, removed case sensitive search

--- starter-code/src/components/ProductTable.js
+++ starter-code/src/components/ProductTable.js
@@ -4,6 +4,20 @@ import ProductRow from './ProductRow'
 class ProductTable extends Component {
     render() {
         const searchTerm = this.props.searchTerm;
+        const rows = []
+
+        this.props.products.forEach(product => {
+            if (product.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                rows.push(
+                    <ProductRow
+                        name={product.name}
+                        price={product.price}
+                        stock={product.stocked ? null : "outOfStock"}
+                        key={product.name}
+                    />
+                );
+            }
+        });
 
         return(
             <table>
@@ -15,9 +29,7 @@ class ProductTable extends Component {
                 </thead>
 
                 <tbody>
-                    { this.props.products.map((item, i) => (
-                        <ProductRow key={i} name={item.name} price={item.price} stock={item.stocked ? null : "outOfStock"} />
-                    )) }
+                    { rows }
                 </tbody>
             </table>
         )
