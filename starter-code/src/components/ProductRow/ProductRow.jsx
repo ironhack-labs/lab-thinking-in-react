667aa6f122951237a9953c4d6a0769eb01859613
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-05-07 17:16:25 +0200

    out-of-stock items are red now

--- starter-code/src/components/ProductRow/ProductRow.jsx
+++ starter-code/src/components/ProductRow/ProductRow.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function ProductRow(props) {
   return (
     <tr>
-      <td>{props.product.name}</td>
+      <td style={{ color: `${props.product.stocked ? 'black' : 'red'}` }}>
+        {props.product.name}
+      </td>
       <td>{props.product.price}</td>
       <td>{props.product.stocked}</td>
     </tr>
