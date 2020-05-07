45406ff8990e47eb2ea8f01f030effe142bd328b
YuvalBu <Yuval.Bu@gmail.com>
2020-05-07 21:08:28 +0200

    Iterations 1-3

---
+++ starter-code/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SearchBar({ searchHandler }) {
+	return (
+		<div>
+			<label>Search Bar</label>
+			<input onChange={searchHandler} type="text" name="Search" />
+		</div>
+	);
+}
