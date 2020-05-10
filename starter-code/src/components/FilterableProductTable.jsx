34046289274d777cecb652066f5c535fd797526f
bdean05 <benjamin_h_dean@yahoo.com>
2020-05-10 17:45:27 +0200

    Sports dollars

---
+++ starter-code/src/components/FilterableProductTable.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import ProductTable from './ProductTable';
+
+class FilterableProductTable extends Component {
+    state = { searchInput: '' };
+
+    searchHandler = (event) => {
+        this.setState({ searchInput: event.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                <SearchBar searchHandler={this.searchHandler} />
+                <ProductTable product={this.props.products} searchValue={this.state.searchInput} />
+            </div>
+        );
+    }
+}
+
+export default FilterableProductTable;
---
+++ starter-code/src/components/ProductRow.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class ProductRow extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.name}</td>
+                <td>{this.props.price}</td>
+            </tr>
+        );
+    }
+}
+
+export default ProductRow;
