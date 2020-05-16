14e552f96e00b6ca1e220e64d39853e558cf16b3
Andre <andre.talpo@gmail.com>
2020-05-16 15:41:44 -0300

    Iteracao 4 incompleta

--- src/components/molecules/SearchBar/SearchBar.js
+++ src/components/molecules/SearchBar/SearchBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const SearchBar = () => {
-    return (
-        <div>
-            <p>Search</p>
-            <input type="text" />
-            <input type="checkbox" id="onStock" />
-            <label htmlFor="onStock">Only show products on stock</label>
-        </div>
-    );
+class SearchBar extends React.Component {
+    state = {
+        value: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Search</p>
+                <input type="text" value={this.state.value} onChange={event => this.onChange(event)} />
+                <input type="checkbox" id="onStock" onClick={(event => this.onClick(event))}/>
+                <label htmlFor="onStock">Only show products on stock</label>
+            </div>
+        );
+    }
+
+    onChange(event) {
+        this.setState({ value: event.target.value },
+            () => this.props.searchMethod(this.state.value));
+    }
+
+    onClick(event) {
+        this.props.filterMethod(event.target.checked);
+    }
 }
 
 export default SearchBar;
--- src/components/organisms/FilterableProductTable/FilterableProductTable.js
+++ src/components/organisms/FilterableProductTable/FilterableProductTable.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { SearchBar, ProductTable } from '../../molecules';
 
-const FilterableProductTable = ({ products }) => {
-    return (
-        <div>
-            <SearchBar />
-            <ProductTable products={products} />
-        </div>
-    );
-};
+class FilterableProductTable extends React.Component {
+    state = {
+        filteredProducts: this.props.products
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log(this.state.filteredProducts)}
+                <SearchBar searchMethod={this.filterProducts} filterMethod={this.filterOnStock} />
+                <ProductTable products={this.state.filteredProducts} />
+            </div >
+        );
+    }
+
+    filterProducts = (name) => {
+        const filteredProducts = { data: this.props.products.data.filter(e => e.name.includes(name)) };
+        this.setState({ filteredProducts });
+    }
+
+    filterOnStock = (checked) => {
+        if (checked) {
+            const filteredProducts = { data: this.state.filteredProducts.data.filter(e => e.stocked) };
+            this.setState({ filteredProducts });
+        } else {
+            this.setState({ filteredProducts: this.props.products });
+        }
+    }
+}
 
 export default FilterableProductTable;
