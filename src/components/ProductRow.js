254598dfe4845ecf8f6eecc18440ce15eb63f6fe
Juli Ramon <juli@nexumlabs.com>
2020-07-21 20:16:25 +0200

    Iteration 1 - Done

---
+++ src/components/ProductRow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductRow = () => {
+  return <div></div>;
+};
+
+export default ProductRow;
---
+++ src/components/ProductTable.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductTable = () => {
+  return <div></div>;
+};
+
+export default ProductTable;
