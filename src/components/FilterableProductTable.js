c7298e255e8fe584fcbc84271cb19007609e3370
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-10-01 22:03:57 -0400

    iteration 1-4

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -6,16 +6,26 @@ export default class FilterableProductTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            products: props.products
+            products: props.products.data,
+            filteredProducts: props.products.data
         }
+    }
+
+    searchFilter = ({search, showInStock}) => {
+        this.setState((state) => ({
+            filteredProducts: state.products.filter(item => 
+                (showInStock || item.stocked) 
+                && item.name.toLowerCase().includes(search.toLowerCase())
+            )
+        }))
     }
 
     render() {
         return (
             <div>
                 <h1>IronStore</h1>
-                <SearchBar />
-                <ProductTable products={this.props.products.data} />
+                <SearchBar onSubmit={this.searchFilter}/>
+                <ProductTable products={this.state.filteredProducts} />
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,8 @@ export default class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ""
+            search: "",
+            showInStock: true
         }
     }
 
@@ -12,6 +13,20 @@ export default class SearchBar extends Component {
         await this.setState({
             search: event.target.value
         })
+
+        this.submit();
+    }
+
+    inStockToggle = async () => {
+        await this.setState({
+            showInStock: !this.state.showInStock
+        })
+
+        this.submit();
+    }
+
+    submit = () => {
+        this.props.onSubmit(this.state);
     }
 
     render() {
@@ -19,7 +34,7 @@ export default class SearchBar extends Component {
             <div>
                 <h3>Search</h3>
                 <input onChange={this.handleChange} type="text" placeholder="search" value={this.state.search}/><br/>
-                <input type="checkbox" id="show-in-stock"/><label name="show-in-stock">Only show in stock products</label><br/>
+                <input onChange={this.inStockToggle} type="checkbox" id="show-in-stock"/><label name="show-in-stock">Only show in stock products</label><br/>
             </div>
         )
     }
