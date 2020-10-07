74e5c418bcff6f0862b4af691e3ac8c6e0cacf9a
Marcha <marchaf@gmail.com>
2020-10-07 23:06:32 +0200

    bux fixes iteration 4

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -1,37 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import ProductTable from './ProductTable';
-import  SearchBar from './SearchBar.jsx';
-
+import SearchBar from './SearchBar.jsx';
 
 class FilterableProductTable extends Component {
-   
-    state = {
-        products: this.props.products,
-        value: "",
-    }
+  state = {
+    products: this.props.products,
+    inStock: false,
+  };
 
-
-    handleSearch =(valueSearch) => {
-    const filteredProducts = this.props.products.filter(product => product.name.toLowerCase().includes(valueSearch.toLowerCase()))
-    this.setState({
-       products: filteredProducts
-     });
+  handleSearch = (valueSearch) => {
+    let filteredProducts;
+    if (typeof valueSearch === 'string') {
+         filteredProducts = this.props.products.filter((product) =>
+        product.name.toLowerCase().includes(valueSearch.toLowerCase())
+      );
+      this.setState({
+        products: filteredProducts,
+      });
+    } else if (typeof valueSearch === 'boolean') {
+          filteredProducts = this.state.products.filter(
+        (product) => product.stocked
+      );
+      this.setState({
+        products: filteredProducts,
+      });
     }
+  };
 
-    render() {
-      
-        return (
-            <div>
-                <h1>IronStore</h1>
-   
-                {/* <SearchBar valueInput={this.state.value} handleSearch={this.handleSearch}  products={this.state.products}/> */}
-                <SearchBar handleSearch={this.handleSearch}/>
-                <ProductTable products={this.state.products} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>IronStore</h1>
+        <SearchBar handleSearch={this.handleSearch} />
+        <ProductTable products={this.state.products} />
+      </div>
+    );
+  }
 }
 
-
-export default  FilterableProductTable;
+export default FilterableProductTable;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,27 +5,38 @@ export default class SearchBar extends Component {
     state = {
         // product : this.props.products,
         searchInput : "",
-    }
+        inStock: false,
+    }   
 
-    handleChange = (evt) =>{
+    handleChange = (evt) =>{ 
         const value = evt.target.value;
         const name = evt.target.name;
         this.setState({
-          [name]: value,
+          [name]: value,        
         });
-      
         this.props.handleSearch(value);
+    }
 
+    handleChecked = (evt) =>{
+        const value =  evt.target.checked;
+        const name = evt.target.name;
+        this.setState({
+          [name]: value,
+           inStock: !this.state.inStock,
+        });
+        console.log(this.state)
+        this.props.handleSearch(value);
     }
 
     render() {
        
         return (
             <div>  
+                
                 <h4>Search</h4>
 
                 <input type="text" name="searchInput" onChange={this.handleChange} value={this.state.searchInput}/>
-                <input type="checkbox"></input>
+                <input type="checkbox" name="inStock" onChange={this.handleChecked} checked={this.state.inStock}></input>
                 <label>Only show products on stock</label>
             </div>
         )
