9d040d1c9ed6a3777a35bb2f7f90cf31837f5072
pheno <nicolas.enault@gmail.com>
2020-10-07 17:10:32 +0200

    is stock

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ProductRow = ({ price, stocked, name }) => {
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{price}</td>
-    </tr>
-  );
-};
+class ProductRow extends Component {
+  state = {
+    isStock: true,
+  };
+
+  render() {
+    return (
+      <tr
+        style={{
+          color: this.state.isStock == this.props.stocked ? 'black' : 'red',
+        }}
+      >
+        <td>{this.props.name}</td>
+        <td>{this.props.price}</td>
+      </tr>
+    );
+  }
+}
 
 export default ProductRow;
