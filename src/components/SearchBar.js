d6d374087b8bce663cd5b7d06d865372626c3991
Timothée Nicole <timothee.nicole@gmail.com>
2020-10-07 18:17:27 +0200

    commit before change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+  handleSearch = (e) => {
+    const val = e.target.value
+  }
+
   render() {
     return (
       <div className="search-bar">
         <label htmlFor="search">Search</label>
-        <input id="search" name="search" type="text" />
+        <input onChange={this.handleSearch} id="search" name="search" type="text" />
         <div className="checkbox-product">
           <input type="checkbox" />
           <label name="stock" htmlFor="productsStock">
