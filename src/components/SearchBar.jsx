4cdb33e39e38af6b83b2ad2e00ccdecc97f0f88f
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-10-07 17:25:37 +0200

    iteration 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  state = {
+    search: '',
+  };
+
+  handlChange = (event) => {
+    const value = event.target.value;
+    const key = event.target.name;
+
+    this.setState({
+      [key]: value,
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Search</h2>
-        <input
-          id="search"
-          autoComplete="off"
-          className="search"
-          type="text"
-          name="search"
-        />
+        <form className="Form" onSubmit={this.handleSubmit}>
+          <input
+            id="search"
+            autoComplete="off"
+            className="search"
+            type="text"
+            name="search"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+        </form>
       </div>
     );
   }
