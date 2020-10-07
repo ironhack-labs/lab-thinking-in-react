7c46ac6021a12b7d45a5be068a61c339c9df45ef
salim ameur <salim.ameur@gmail.com>
2020-10-07 18:17:05 +0200

    iteration 3

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -5,16 +5,45 @@ import data from '../data.json';
 
 export default class FilterableProductTable extends Component {
   state = {
-    allProducts: data,
+    allProducts: data.data,
   };
+ handleNewSearch=(search)=> {
+if(!search){
+  this.setState({
+    allProducts:data.data
+  })
+  
+}else{
 
-  render() {
+
+   console.log(search)
+
+   const copyData = [...this.state.allProducts]
+
+  // console.log(copyData[0].name)
+this.setState({ 
+  
+  allProducts:copyData.filter((obj,i)=> {
+
+  return(
+  obj.name.includes(search))
+
+
+})})}
+
+
+
+ }
+
+ 
+  render() 
+  { console.log(this.state.allProducts)
     return (
       <div>
         <center>
           <h1>IronStore</h1>
         </center>
-        <SearchBar></SearchBar>
+        <SearchBar handleSearch={this.handleNewSearch} ></SearchBar>
         <ProductTable productList = {this.state.allProducts} />
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+  state ={
+    search : ""
+  }
+ 
+  handleChange =(event)=>{
+    // console.log(event.target.value);
+
+    let value = event.target.value;
+    // const key = event.target.name;
+    
+    this.setState({search:value})
+   this.props.handleSearch(value)
+
+  }
+
   render() {
     return (
       <div className="search-bar">
         <label htmlFor="search">Search</label>
-        <input id="search" name="search" type="text" />
+        <input onChange={this.handleChange} id="search" name="search" type="text" />
         <div className="checkbox-product">
           <input type="checkbox" />
           <label name="stock" htmlFor="productsStock">
