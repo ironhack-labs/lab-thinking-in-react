aff6dd5eff9a02894152c0d007c6a90ef9a5e093
Lisa Poncet <poncet.lisa@gmail.com>
2020-10-07 17:36:34 +0200

    iteration 5

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -11,9 +11,8 @@ export default class Searchbar extends Component {
     const value =
       e.target.type === 'checkbox' ? e.target.checked : e.target.value;
 
-    this.setState(
-      {[name]: value},
-      () => this.props.searchProduct(this.state)
+    this.setState({ [name]: value }, () =>
+      this.props.searchProduct(this.state)
     );
   };
 
@@ -27,13 +26,15 @@ export default class Searchbar extends Component {
             onChange={this.handleChange}
             value={this.state.searchbar}
           />
-          <input
-            type="checkbox"
-            name="inStock"
-            onChange={this.handleChange}
-            checked={this.state.inStock}
-          />
-          <label>Only show products on stock</label>
+          <div>
+            <input
+              type="checkbox"
+              name="inStock"
+              onChange={this.handleChange}
+              checked={this.state.inStock}
+            />
+            <label>Only show products on stock</label>
+          </div>
         </form>
       </div>
     );
