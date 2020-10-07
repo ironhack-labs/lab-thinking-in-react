13371454f421b85862ba2621f7b2ee7c7505bda6
Marcha <marchaf@gmail.com>
2020-10-07 16:00:42 +0200

    Iteration 1 done

---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import data from '../data.json';
+
+export default class FilterableProductTable extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default  FilterableProductTable;
