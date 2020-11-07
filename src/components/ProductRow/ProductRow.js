008267981552f477d3e687e8103bacf4c23820fe
unimatrix2 <dnny.nis@gmail.com>
2020-11-07 12:32:22 -0300

    cleaned td class conditional

--- src/components/ProductRow/ProductRow.js
+++ src/components/ProductRow/ProductRow.js
@@ -5,7 +5,7 @@ class ProductRow extends Component {
     render() {
         return(
         <tr key={this.props.idKey}>
-            {this.props.stocked ? <td className='py-4'>{this.props.name}</td> : <td className='has-text-danger py-4'>{this.props.name}</td>}
+            <td className={this.props.stocked ? 'py-4' : 'py-4 has-text-danger'} >{this.props.name}</td>
             <td className='py-4'>{this.props.price}</td>
             <td className='py-4'>{this.props.cat}</td>
         </tr>
