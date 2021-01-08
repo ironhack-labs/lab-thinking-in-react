084ea666cfc1236ec0f1cc0655cde9d0e811e150
Alicja Slupska <ala.slupska@gmail.com>
2021-01-08 20:29:10 +0100

    Iteration 4 done

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -7,23 +7,32 @@ import SearchBar from './SearchBar';
 class FilterableProductTable extends React.Component {
 
     state = {
-        searchTerm: ""
+        searchTerm: "",
+        // is the checkbox active (clicked) or not
+        filterInStockActive: false
     }
 
     handleSearchTerm = (newTerm) => {
         this.setState({ searchTerm: newTerm });
     }
 
+    handleCheckBox = (event) => {
+        this.setState({ filterInStockActive: event.currentTarget.checked });
+    }
+
     dynamicSearch = () => {
-        return this.props.products.filter(product => product.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
-      }
+        return this.props.products
+            // if the filterInStockActive is false, then show all products OR (if it is true) show only stocked products
+            .filter(product => !this.state.filterInStockActive || product.stocked)
+            .filter(product => product.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
+    }
 
 
     render() {
         return (
             <div className="FilterableTable">
                 <h1>IronStore</h1>
-                <SearchBar value={this.state.searchTerm} onChange={this.handleSearchTerm} />
+                <SearchBar value={this.state.searchTerm} filterInStockActive={this.state.filterInStockActive} onChange={this.handleSearchTerm} checkBoxChange={this.handleCheckBox} />
                 {/* props passing data down */}
                 {/* <ProductTable products={this.props.products} /> */}
                 <ProductTable products={this.dynamicSearch()} />
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,7 +3,12 @@ import React from 'react';
 const SearchBar = (props) => {
     return (
         <div>
-            <input type="text" value={props.value} onChange={event => props.onChange(event.currentTarget.value)} placeholder="Search products..."></input>
+            <input type="text" value={props.value} onChange={event => props.onChange(event.currentTarget.value)} placeholder="Search products..." className="search-input"></input>
+            <div>
+                <input type="checkbox" id="on-stock" checked={props.filterInStockActive} onChange={props.checkBoxChange} ></input>
+                <label htmlFor="on-stock" >Only show products on stock</label>
+            </div>
+
         </div>
     )
 }
