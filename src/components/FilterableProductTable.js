c2cb59edd7fdc17b9990f445e1ddd3139e5b5f24
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-02-03 13:42:47 +0100

    complete iteration 3

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -9,11 +9,20 @@ class FilterableProductTable extends React.Component {
             products: this.props.products
         }
     }
+
+    handleSearch = (value) => {
+        const filteredProducts = value ? this.state.products.filter(product => (product.name.toUpperCase().includes(value.toUpperCase()))) : this.props.products
+
+        this.setState({
+            products: filteredProducts
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>IronStore</h1>
-                <SearchBar />
+                <SearchBar searchProducts={this.handleSearch} />
                 <ProductTable products={this.state.products} />
             </div>
         )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             searchInput: ''
         }
     }
+
+    handleSearchInput = (e) => {
+        const value = e.target.value
+        this.setState({
+            searchInput: value
+        })
+        this.props.searchProducts(value)
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    <label>Search</label>
-                    <input type="text" value={this.state.searchInput} />
+                    <label for="search">Search</label>
+                    <input type="text" name="search" id="search" value={this.state.searchInput} onChange={this.handleSearchInput} />
                 </form>
             </div>
         )
