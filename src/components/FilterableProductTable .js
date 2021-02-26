e5f767eb71f3d958b0502e36f79233b62372ca73
Sebastian Septien <sebsepdus@hotmail.com>
2021-02-25 22:30:05 -0600

    the search bar was fixed

--- src/components/FilterableProductTable .js
+++ src/components/FilterableProductTable .js
@@ -3,11 +3,23 @@ import ProductTable from './ProductTable'
 import SearchBar from './SearchBar'
 
 export default function FilterableProductTable (props) {
-    const  [prod,setProd] = useState(props.products);
+    const  [product, setProduct] = useState(props.products);
+    const [checkedBox, setcheckedBox] = useState(false);
+    const [typeWord,setTypeWord] = useState("")
+    function filterProd(){
+        return props.products.filter((product) => {
+            return (
+              (checkedBox ? product.stocked : true) &&
+              product.name.toLowerCase().includes(typeWord.toLowerCase()) 
+            );
+          });
+    }
+
+    const prod= filterProd();
     return (
         <div>
-            <SearchBar products={prod} setproducts={setProd} />
-            <ProductTable products={prod} setproducts={setProd}/>
+            <SearchBar setTypeWord={setTypeWord} checkedBox={checkedBox} setcheckedBox={setcheckedBox} />
+            <ProductTable products={prod} />
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,13 @@
 import React, { useState } from 'react';
 
 export default function SearchBar(props) {
-  const [foodCopy, setFoodCopy] = useState(props.products);
-  let [stock, setStock] = useState(false);
 
   function handleChange(event) {
     if (event.target.name === 'stock') {
-      setStock(stock ? false : true);
+      props.checkedBox ?props.setcheckedBox(false) : props.setcheckedBox(true);
     } else {
-        console.log(stock);
-      const filterFood = foodCopy.filter((food) => {
-        return (stock ? food.stocked : true) && food.name
-        .toLowerCase()
-        .includes(event.target.value.toLowerCase());
-      });
-      props.setproducts(filterFood);
-    
+      props.setTypeWord(event.target.value);
     }
-  }
-
-function checkBox() {
-    const filterFood = foodCopy.filter((food) => {
-      if (!stock) {
-        return food.stocked === true;
-      }
-      return true;
-    });
-    props.setproducts(filterFood);
   }
 
   return (
@@ -35,11 +16,10 @@ function checkBox() {
       <input name="search" type="text" onChange={(e) => handleChange(e)} />
       <form>
         <input
-         onClick={()=>checkBox()}
           onChange={(e) => handleChange(e)}
           type="checkbox"
           name="stock"
-          value={stock}
+          value={props.checkedBox}
         />
       </form>
       <label>Only show products on stock</label>
