d84b03bcde58d414a9a418fdd9abf4dd2f8b4dc9
Diego <diego.arronizt@gmail.com>
2021-02-25 22:10:52 -0500

    Terminado hasta media iteracion de bonus

---
+++ src/components/ProductTable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductRow from './ProductRow'
+
+export default function ProductTable(props) {
+    return (
+        <div>
+            <ProductRow articulos = {props.articulos}/>
+        </div>
+    )
+}
