e7d8d7877c430fa124a935ef001a0f8f4dfb0d06
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-25 22:40:15 -0600

    done

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductTable from './ProductTable';
+
+export default function FilterableProductTable() {
+
+    return (
+        <div>
+            <ProductTable />
+        </div>
+    )
+}
