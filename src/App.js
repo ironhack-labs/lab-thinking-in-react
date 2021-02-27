e91096c792c13c4bb10205299cfc4c12e5e153d8
Sebastian Montoya <seb246@gmail.com>
2021-02-27 18:50:38 -0500

    restructured doing another way

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import FilterableProductTable from './components/FilterableProductTable';
 import jsondata from './data.json';
 class App extends React.Component {
+  state = {
+    products: jsondata.data,
+  };
+
+  showProducts = () => {
+    return this.state.products.map((eachProduct) => {
+      return (
+        <tr>
+          <td>{eachProduct.name} </td>
+          <td>{eachProduct.price}</td>
+        </tr>
+      );
+    });
+  };
+
+  search = (e) => {
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <div className="App">
-        <FilterableProductTable products={jsondata.data} />
+        <label>
+          Search:
+          <input name="search" type="text" onChange={this.search} />
+        </label>
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Price</th>
+          </thead>
+          <tbody>{this.showProducts()}</tbody>
+        </table>
       </div>
     );
   }
