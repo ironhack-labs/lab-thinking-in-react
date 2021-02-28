1bd139ca1e691dbca37276f88824621cf8e84361
Sebastian Montoya <seb246@gmail.com>
2021-02-27 23:09:01 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import jsondata from './data.json';
 class App extends React.Component {
   state = {
     products: jsondata.data,
+    hideMe: true,
   };
 
   showProducts = () => {
     return this.state.products.map((eachProduct) => {
       return (
-        <tr>
+        <tr
+          hidden={this.state.hideMe && !eachProduct.stocked}
+          key={eachProduct.id}
+          className={!eachProduct.stocked && 'noStock'}
+        >
           <td>{eachProduct.name} </td>
           <td>{eachProduct.price}</td>
         </tr>
@@ -18,6 +23,21 @@ class App extends React.Component {
 
   search = (e) => {
     console.log(e.target.value);
+    let filteredCopy = jsondata.data.filter((eachProduct) => {
+      return eachProduct.name
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+    this.setState({ products: filteredCopy });
+  };
+
+  showStock = () => {
+    console.log('clicked');
+    let filteredCopy = jsondata.data.filter((eachProduct) => {
+      return eachProduct.stocked;
+    });
+
+    this.setState({ products: filteredCopy });
   };
 
   render() {
@@ -26,6 +46,16 @@ class App extends React.Component {
         <label>
           Search:
           <input name="search" type="text" onChange={this.search} />
+        </label>
+        <label>
+          Show only in stock
+          <input
+            type="checkbox"
+            name="inStock"
+            onChange={() => {
+              this.setState({ hideMe: !this.state.hideMe });
+            }}
+          />
         </label>
         <table>
           <thead>
