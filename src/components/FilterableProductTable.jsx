db8af4298d11b9fa4750d8f5659bd3703921d4cd
Cl Sayart <clsayart@gmail.com>
2021-03-18 17:49:31 +0100

    done

---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import SearchBar from './SearchBar'
+import ProductTable from './ProductTable'
+
+class FilterableProductTable extends Component {
+    render() {
+      return (
+        <div>
+          <SearchBar />
+          <ProductTable products={this.props.products}/>
+        </div>
+      );
+    }
+  }
+
+export default FilterableProductTable;
