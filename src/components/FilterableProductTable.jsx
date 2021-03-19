5a9cebbc47293cb476515b3af2b2b60a4a640a97
Mohammed Adib <adibmohammed95@gmail.com>
2021-03-20 00:29:15 +0100

    Lab coincided with App review

---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class FilterableProductTable extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FilterableProductTable
