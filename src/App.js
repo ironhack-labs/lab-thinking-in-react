847156bed1e3699d6ec9386feb123713bfa1a1a8
monteromario <59968975+monteromario@users.noreply.github.com>
2021-04-10 00:17:57 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import jsondata from './data.json'
+import React, { Component } from 'react';
+import jsondata from './data.json';
 import './App.css';
 import ProductTable from './components/ProductTable';
 import SearchBar from './components/SearchBar';
 
-function App() {
-  let data = jsondata.data
-  return (
-    <div className="App mx-5 my-4">
-      <h1>IronStore</h1>
-      <SearchBar />
-      <ProductTable products={data} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: jsondata.data,
+    search: '',
+    stock: false
+  }
+
+  updateSearch = (search) => {
+    this.setState({search: search})
+  }
+
+  updateCheck = (checked) => {
+    checked ? this.setState({stock: true}) : this.setState({stock: false})
+  }
+
+  render() {
+  let filteredData
+  this.state.search !== '' ? filteredData = this.state.data.filter(product => product.name.toLowerCase().includes(this.state.search.toLowerCase())) : filteredData = this.state.data
+  this.state.stock ? filteredData = [...filteredData].filter(product => product.stocked) : filteredData = [...filteredData]
+    return (
+      <div className="App mx-5 my-4">
+        <h1>IronStore</h1>
+        <SearchBar handleInput={this.updateSearch} handleCheck={this.updateCheck}/>
+        <ProductTable products={filteredData} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,28 +3,28 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form'
 
-
-function SearchBar() {
+let SearchBar = ({ handleInput, handleCheck }) => {
   return (
     <div className="m-4 w-50 ml-auto mr-auto">
         <Form>
             <InputGroup className="mb-3">
                 <InputGroup.Prepend>
-                <InputGroup.Text id="search"> <span role="img" aria-label="search">🔎</span> </InputGroup.Text>
+                    <InputGroup.Text id="search"> <span role="img" aria-label="search">🔎</span> </InputGroup.Text>
                 </InputGroup.Prepend>
-                <FormControl
-                placeholder="Seach items"
-                aria-label="search"
-                aria-describedby="search"
-                />
+                    <FormControl
+                    placeholder="Seach items"
+                    aria-label="search"
+                    aria-describedby="search"
+                    id="search"
+                    onChange={(e) => handleInput(e.target.value)}
+                    />
             </InputGroup>
-            <Form.Group controlId="checkbox">
-                <Form.Check type="checkbox" label="Only show products on stock" />
-            </Form.Group>
+                <Form.Group controlId="checkbox">
+                    <Form.Check type="checkbox" label="Only show products on stock" id="check" onChange={(e) => handleCheck(e.target.checked)}/>
+                </Form.Group>
             </Form>
     </div>
   )
-  
 }
 
 export default SearchBar;
