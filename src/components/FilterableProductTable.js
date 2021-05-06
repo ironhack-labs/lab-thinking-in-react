5a48d8de30cbdd1abd792f1736871ed151abb887
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-06 16:47:08 +0200

    almost finished with errors to do with checkbox

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -12,27 +12,42 @@ export default class FilterableProductTable extends Component {
 
     changeHandler = (event)=>{
         // console.log('change handler',event)
-        
-        var filtered = this.props.products.filter((product)=>{
-            return product.name.toLowerCase().includes(event)
-        })
+
+      
+
+        const value = event.type === 'checkbox' ? event.checked : event.value;
+
+        const name = event.name
         this.setState({
-            search: event,
+            [name]: value,
             filter: filtered
         })
-        console.log(filtered)
+        var filtered = this.props.products.filter((product)=>{
+            return product.name.toLowerCase().includes(event.value) && product.stocked === event.checked
+        })
+        this.setState({
+            filter : filtered
+        })
+    }
+
+    checkHandler = (event)=>{
+        console.log(event)
+        this.setState({
+            inStock : event
+        })
     }
     
   state = {
     search: "",
     filter: this.props.products,
+    stocked: false
     // data: this.props.products,
   };
   render() {
     return (
       <div className="FilterableProductTable">
         <p>hello</p>
-        <SearchBar search={this.state.search} submitHandler={this.submitHandler} changeHandler={this.changeHandler}/>
+        <SearchBar search={this.state.search} inStock={this.state.stocked} submitHandler={this.submitHandler} changeHandler={this.changeHandler} checkHandler={this.checkHandler}/>
         <ProductTable products={this.state.filter}  />
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,15 +5,25 @@ export default class SearchBar extends Component {
     //     props.changeHandler(event)
     // }
     handleChange = event =>{
-        this.props.changeHandler(event.target.value)
+        this.props.changeHandler(event.target)
+    }
+
+    handleCheck = event =>{
+        this.props.checkHandler(event.target.checked)
+    }
+
+    handleSubmit = event =>{
+        this.props.submitHandler(event.target.value)
     }
  render(){
     
     return (
         <div className="SearchBar">
-          <form onSubmit="">
+          <form onSubmit={this.handleSubmit}>
             <label htmlFor="search">Search</label>
             <input type="text" name="search" id="search" value={this.props.search} onChange={this.handleChange} />
+            <label htmlFor="stocked">Only see In stock items</label>
+            <input type="checkbox" name="stocked" id="stocked" checked={this.props.inStock} onChange={this.handleChange}/>
           </form>
         </div>
       );
