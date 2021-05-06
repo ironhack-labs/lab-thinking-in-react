1fa892ae1b01e2a6f33e43aa10412339db2ab145
Hector <Thornnk1@gmail.com>
2021-05-06 12:43:47 +0200

    Done Iteration 4

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -7,7 +7,8 @@ class FilterableProductTable extends React.Component{
 
     state = {
         products: this.props.products,
-        comparativeProducts: this.props.products
+        comparativeProducts: this.props.products,
+        checked: false
     }
 
     filterSearch(e){
@@ -18,6 +19,20 @@ class FilterableProductTable extends React.Component{
         this.setState({...this.state, comparativeProducts: filteredProducts})
     }
 
+    changeCheck(){
+        if (this.state.checked){
+            const filteredProducts = this.state.products.filter((product)=>{
+                return product
+            })
+            this.setState({...this.state, comparativeProducts: filteredProducts, checked: false})
+        } else {
+            const filteredProducts = this.state.products.filter((product)=>{
+                return product.stocked
+            })
+            this.setState({...this.state, comparativeProducts: filteredProducts, checked: true})
+        }
+    }
+
     render(){
         return(
             <div className="FilterableProductTable">
@@ -26,6 +41,9 @@ class FilterableProductTable extends React.Component{
                 products={this.state.products}
                 filterSearch={(data)=>this.filterSearch(data)}
                 />
+
+                <input onChange={()=>this.changeCheck()} type="checkbox" name="onlyStock"/>
+                <label htmlFor="onlyStock">Only show products on stock</label>
 
                 <ProductTable 
                 products={this.state.comparativeProducts}
