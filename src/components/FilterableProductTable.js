03e7496028870d126ecc70046d3ce065d58ecadd
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-06 14:51:47 +0200

    add search table

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -4,8 +4,27 @@ import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 
 export default class FilterableProductTable extends Component {
+    submitHandler = (event) =>{
+        event.preventDefault();
+        console.log('submit handler')
+       
+    }
+
+    changeHandler = (event)=>{
+        // console.log('change handler',event)
+        
+        var filtered = this.props.products.filter((product)=>{
+            return product.name.toLowerCase().includes(event)
+        })
+        this.setState({
+            search: event,
+            filter: filtered
+        })
+        console.log(filtered)
+    }
     
   state = {
+    search: "",
     filter: this.props.products,
     // data: this.props.products,
   };
@@ -13,8 +32,8 @@ export default class FilterableProductTable extends Component {
     return (
       <div className="FilterableProductTable">
         <p>hello</p>
-        <SearchBar />
-        <ProductTable products={this.state.filter}/>
+        <SearchBar search={this.state.search} submitHandler={this.submitHandler} changeHandler={this.changeHandler}/>
+        <ProductTable products={this.state.filter}  />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function SearchBar(props){
-
-    return(
+export default class SearchBar extends Component {
+    // changeHandler = event=>{
+    //     props.changeHandler(event)
+    // }
+    handleChange = event =>{
+        this.props.changeHandler(event.target.value)
+    }
+ render(){
+    
+    return (
         <div className="SearchBar">
-<p>Hi from the searchbar   </p>
+          <form onSubmit="">
+            <label htmlFor="search">Search</label>
+            <input type="text" name="search" id="search" value={this.props.search} onChange={this.handleChange} />
+          </form>
         </div>
-    )
+      );
+ }
+  
 }
