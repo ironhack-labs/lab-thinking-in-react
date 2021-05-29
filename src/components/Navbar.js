51b4b8778a73a2e261e3df925d6b46913ea40f6a
danpinon <danielpformoso@gmail.com>
2021-05-29 13:42:31 -0500

    update build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div className='navbar navbar-expand-lg navbar-dark bg-primary'>
-            <a class="navbar-brand" href="#">Amazens</a>
+            <a class="navbar-brand" href="/">Amazens</a>
         </div>
     )
 }
