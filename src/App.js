66094deb0c415a022471a12bd3535c91bc2472f0
Bart Spangenberg <bart_spangenberg@protonmail.com>
2021-07-15 20:37:29 +0200

    full lab --done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Searchbar from './components/Searchbar';
+import ProductTable from './components/ProductTable';
+import productData from './data.json'
 import './App.css';
 
 function App() {
+  
+  const [ products, setProducts ] = useState(productData.data);
+  const [ filteredProducts, setFilteredProducts ] = useState(productData.data);
+  const [ searchText, setSearchText ] = useState('');
+  const [ displayStock, setDisplayStock ] = useState(true);
+
+  useEffect (() => {
+    filterProducts();
+  }, [displayStock, searchText]) 
+
+  const handleSearch = event => {
+    const searchText = event.target.value.toLowerCase();
+    setSearchText(searchText);
+  }
+  
+  const handleDisplayStock = event => {
+    const displayStock = event.target.checked;
+    setDisplayStock(displayStock);
+  }
+  
+  const filterProducts = () => {
+    let newFilteredProducts = products;
+    if (displayStock === false) {
+      newFilteredProducts = products.filter(product => product.stocked);
+    }
+    newFilteredProducts = newFilteredProducts.filter(product => product.name.toLowerCase().includes(searchText));
+    setFilteredProducts(newFilteredProducts);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ display: "flex", justifyContent: "center"}}>IronStore</h1>
+      <Searchbar onSearch={handleSearch} onDisplayStock={handleDisplayStock} />
+      <ProductTable products={filteredProducts}/>
     </div>
   );
 }
