364e7126519562475407bb99c068c1b29db9ea26
Irina <ifalko@protonmail.com>
2021-07-15 11:32:28 +0200

    Components added, structure created

---
+++ src/ProductTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ProductRow from './ProductRow.js';
+
+export default class ProductTable extends Component {
+  render() {
+    return (
+      <>
+        <p>Here is the ProductTable</p>
+        <ProductRow />
+      </>
+    );
+  }
+}
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <>
+        <p>Here is the search area</p>
+      </>
+    );
+  }
+}
