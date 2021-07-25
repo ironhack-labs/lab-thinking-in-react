ee32d76bdc433098b761c45567f6fa56b5622cea
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-07-25 17:57:21 -0300

    done

---
+++ src/components/FilterableProductTable/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import SearchBar from '../SearchBar/index';
+import ProductTable from '../ProductTable/';
+
+class FilterableProductTable extends Component {
+  
+    state = {
+    products: this.props.products.data,
+    foundProducts: this.props.products.data,
+  };
+
+  searchHandler = (query) => {
+    console.log(query);
+    const foundProducts = this.state.products.filter((product) =>
+      product.name.includes(query)
+    );
+    this.setState({ foundProducts: foundProducts });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="h1 p-2">IronStore</h1>
+        <SearchBar searchQuery={this.searchHandler} />
+        <ProductTable products={this.state.foundProducts} />
+      </div>
+    );
+  }
+}
+
+export default FilterableProductTable;
