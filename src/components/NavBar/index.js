214287a4ce9a661ad630d7d7bf6a8271a5c75368
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-07-29 16:00:56 -0300

    done

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div class="navbar is-dark p-2">
-    <div class="navbar-start ">
+    <div className="navbar is-dark p-2">
+    <div className="navbar-start ">
     <h1 className='is-size-3 ml-5'>IronStore</h1>
     </div>
   
