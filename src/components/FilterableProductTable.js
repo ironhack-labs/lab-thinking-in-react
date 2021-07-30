c1d1c9a6a39f342ea282db17ea217b1cc0ad01fc
Catuxa.aljo <catuxa.aljo@gmail.com>
2021-07-30 17:27:16 +0200

    It. 1 to 4

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,17 +1,42 @@
 import React from 'react'
+import { Component } from 'react'
 import ProductTable from "./product/ProductTable"
 import SearchBar from './search-bar/SearchBar'
 
 
-function FilterableProductTable ({ products }) {
+class FilterableProductTable extends Component {
+
+    state= {
+        products: [...this.props.products]
+    }
     
+    handleSearchProducts(event) {
+        const { value } = event.target  
+        this.setState(({products}) => ({
+           products : products.filter(product => product.name.toLowerCase().includes(value))
+        }))
+    }
+
+    handleSearchAvalaibleProducts(event) {
+        const {value} = event.target
+        this.setState(({products}) => ({
+
+            products : products.filter(product => product.stocked)
+
+        }           
+        ))
+    }
+
+    render(){
+    const { products } = this.state
 
     return(
         <div>
-        <SearchBar />
+        <SearchBar onSearch= {(event) => this.handleSearchProducts(event)} onProducstAvalaible={(event) => this.handleSearchAvalaibleProducts(event) } />
         <ProductTable products= {products} />
         </div>
-    )
+
+    )}
 }
 
 export default FilterableProductTable
--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -3,6 +3,13 @@ import React from 'react'
 import { Component } from 'react';
 
 class SearchBar extends Component {
+    
+    state = {
+        product: {
+            searchproduct:'',
+            avalaibleonly: false
+        }
+    }
 
     handleInputs(event){
         const {name, value } = event.target
@@ -13,12 +20,21 @@ class SearchBar extends Component {
     }
 
     render(){
+        const { onSearch, onProducstAvalaible } = this.props
+        const { searchproduct } = this.state
         return(
-
+            <form>
             <div class="input-group mb-3">
-                <input type="text" name="search" class="form-control"  placeholder="Search for a product" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                <input type="text" name="search" class="form-control" value={searchproduct} onChange={(event) => this.handleInputs(event), onSearch} placeholder="Search for a product" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
                 <span class="input-group-text" id="basic-addon2"><i class="fas fa-search"></i></span>
             </div>
+            <div class="form-check">
+                <input class="form-check-input" name="avalaibleonly" type="checkbox" value={true} id="flexCheckDefault" onChange={(event) => this.handleInputs(event), onProducstAvalaible}/>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Show only products in stock
+                </label>
+            </div>
+            </form>
 
         )
     }
