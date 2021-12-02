4da3a5b3a1ea435673a681da71684c4ee8afbd0b
JoseLValdes <jolvaldgo@gmail.com>
2021-12-02 12:03:19 +0100

    component FilterableProductTable

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilterableProductTable = (props) => {
+  return (
+    <div>
+      <h2>FilterableProductTable</h2>
+    </div>
+  );
+};
+
+export default FilterableProductTable;
