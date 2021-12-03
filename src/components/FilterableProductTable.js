30b295cfea47012f09777230dd5ad6d64a9e3b40
JoseLValdes <jolvaldgo@gmail.com>
2021-12-03 10:27:43 +0100

    Completed

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
 const FilterableProductTable = (props) => {
+  let [filteredProducts, setfilteredProducts] = useState([...props.products]);
+  function filterBy(word){
+    setfilteredProducts(props.products.filter((product)=> product.name.toLowerCase().includes(word.toLowerCase()) ));
+
+  }
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar />
-      <ProductTable  products={ props.products } />
+      <SearchBar filterBy={filterBy}   />
+      <ProductTable  products={ filteredProducts }  />
     </div>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,14 @@ const SearchBar = (props) => {
   return (
     <div>
       <h2>Search</h2>
+
       <input
         type="text"
         onChange={(event) => props.filterBy(event.target.value)}
+        name="search"
       />
       <input type="checkbox" id="products" name="products" />
-      <label for="products"> Only show products on Stock</label>
+      <label htmlFor="products"> Only show products on Stock</label>
     </div>
   );
 };
