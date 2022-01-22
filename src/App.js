6893131f00ed3b63e5143ef2928d4835f2802a3e
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-22 11:55:03 +0100

    iteration 0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import productsJson from "./data.json";
 
 function App() {
   return (
     <div className="App">
-      
+      {/* {productsJson.map((product) => (
+        <div>{product.price}</div>
+      ))} */}
     </div>
   );
 }
 
 export default App;
+// {
+//   "category": "Sporting Goods",
+//   "price": "$49.99",
+//   "inStock": true,
+//   "name": "Football"
+// }
