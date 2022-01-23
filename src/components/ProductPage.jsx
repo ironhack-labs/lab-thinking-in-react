d3626d19496d2087d764e84c9906786aa59407dd
Philipp Kastl <picastle@posteo.de>
2022-01-23 12:46:14 +0100

    iteration 1

---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import jsonData from './../../data.json'
+
+
+const ProductPage = () =>{
+
+const [products, useProducts] = React.useState(jsonData)
+
+
+return(
+    <div className="ProductName">
+
+    </div>
+    )
+}
+
+export default ProductPage
