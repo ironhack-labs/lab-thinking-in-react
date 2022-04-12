2362290eb5c973e9b4d1f6a68d5135d4bdde21fa
Fer Sánchez <sangarfer@gmail.com>
2022-04-12 16:32:49 +0200

    filter done

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -5,13 +5,24 @@ import SearchBar from './SearchBar';
 
 function ProductsPage () {
   const [products, setProducts] = useState(jsonData);
+  const [copiedProducts, setCopiedProducts] = useState(jsonData);
+
+  function filterProducts(string){
+    const filteredProducts = products.filter((product)=>{
+
+        if(string === "") return true;
+        else return product.name.toLowerCase().includes(string.toLowerCase());
+      })
+  
+      setCopiedProducts(filteredProducts);
+  }
   
   return(
       <div>
         <h1>IronStore</h1>
-        <SearchBar />
-        <ProductTable products={products} setProducts={setProducts}/>
-      </div>    
+        <SearchBar filterProducts={filterProducts}/>
+        <ProductTable products={copiedProducts}/>
+      </div>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,17 @@
-export default function SearchBar (){
+import { useState } from "react";
+
+export default function SearchBar (props){
+
+    const [searchBarData, setSearchBarData] = useState("");
+
+    function handleSearch(event){
+        const value = event.target.value;
+        setSearchBarData(value)
+
+        props.filterProducts(value);
+    }
+
     return (
-        <input className="searchbar" type="search" />
+        <input className="searchbar" name="search-bar" value={searchBarData} type="search" onChange={handleSearch}/>
     )
 }
