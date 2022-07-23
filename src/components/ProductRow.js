2eced1007246e09d2c44fab5a61cb4c58375196e
Randolph Roy <randolphroy@me.com>
2022-07-23 19:25:28 -0400

    RMT-WEBDEV-JUN2022-RANDOLPH ROY

---
+++ src/components/ProductRow.js
@@ -0,0 +1,19 @@
+function ProductRow(props) {
+
+    const { name, price, inStock} = props;
+
+    return (
+        <tr>
+            <td style = {{
+                color: inStock ? 'black' : 'red'
+            }}>
+                {name}
+            </td>
+            <td>
+                {price}
+            </td>
+        </tr>
+    )
+}
+
+export default ProductRow;
