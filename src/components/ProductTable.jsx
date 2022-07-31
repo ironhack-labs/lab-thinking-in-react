1f3e47d2c5861c64f5c98465d0d2d02479dedff8
Vale Quiroga <valentinqramos@outlook.com>
2022-07-31 17:51:07 -0500

    Iteration 4

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,6 +1,18 @@
-const ProductTable = ({products}) => {
+import { useState } from 'react';
+
+const ProductTable = ({ products }) => {
+  const [searchName, setSearchName] = useState('');
   return (
     <div>
+      <input
+        placeholder="Search..."
+        type="text"
+        onChange={(e) => {
+          setSearchName(e.target.value);
+        }}
+      />
+      <br />
+      <input type="checkbox" /> Only Show Products in Stock
       <table>
         <thead>
           <tr>
@@ -9,17 +21,33 @@ const ProductTable = ({products}) => {
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td style={product.inStock ? {color:'black'} : {color:'red'}} > {product.name} </td>
-              <td> {product.price} </td>
-            </tr>
-              
-          ))}
+          {products
+            .filter((value) => {
+              if (searchName === '') {
+                return value;
+              } else if (
+                value.name.toLowerCase().includes(searchName.toLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((product) => (
+              <tr key={product.id}>
+                <td
+                  style={
+                    product.inStock ? { color: 'black' } : { color: 'red' }
+                  }
+                >
+                  {' '}
+                  {product.name}{' '}
+                </td>
+                <td> {product.price} </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default ProductTable;
