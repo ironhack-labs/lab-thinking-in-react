69565c086ac59a8056b6a532f3b999dbee254e6c
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-08-09 18:23:41 -0500

    iteracion4 done

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,23 +1,39 @@
 import { useState } from 'react';
 import jsonData from '../data.json';
 import ProductTable from './ProductTable'
-import SearchBar from './SearchBar'
+
+//    "name": "Football'
 
 function ProductsPage () {
 
   const [products, setProducts] = useState(jsonData);
+  const [filterCustom, setFilter] = useState('');
 
-  console.log(products);
+  const handlerSearch = (e)=>{
+setFilter(()=>e.target.value)
+  }
+
+  const productsFiltered = products.filter(product=>product.name.toLowerCase().includes(filterCustom.toLowerCase()))
+  
+
+  console.log("productos filtrados o sin filtrar",productsFiltered );
 
     return (
+
         <div>
 
 
+      <div>
+        <h1>IronStore</h1>
+        <input type="text" placeholder="Search..." onChange={(e=>handlerSearch(e))} />
+      </div>
+  
 
-<ProductTable data={products}/>
+
+<ProductTable data={productsFiltered}/>
 
 
-<SearchBar />
+
 
 </div>
     )
--- src/components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-function SearchBar() {
-    return (
-      <div>
-        <h1>IronStore</h1>
-      </div>
-    );
-  }
-  export default SearchBar;
-  
