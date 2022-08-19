f76fa78497f23415b3158f0c7ef8a7efacf30460
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-19 16:12:13 -0300

    fazendo iteração 3

---
+++ src/components/ProductRow.js
@@ -0,0 +1,15 @@
+import './ProductRow.css'; 
+
+function ProductRow () {
+    return (
+        <table className='table-body'>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>     
+    )
+}
+
+export default ProductRow;
--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -2,15 +2,24 @@ import { useState } from 'react';
 import jsonData from '../data.json';
 import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
+import ProductRow from './ProductRow';
 
 function ProductsPage () {
 const [ products, setProducts ] = useState(jsonData)
+
+let row = products.map((eachproduct) => {
+    return  <div key={eachproduct.id} className='products'>
+                {eachproduct.name}
+                {eachproduct.price}
+            </div>
+})
 
     return (
         <div>
             <h1>IronStore</h1>
             <SearchBar />
             <ProductTable />
+            <ProductRow row={row}/>
         </div>
     )
 }
