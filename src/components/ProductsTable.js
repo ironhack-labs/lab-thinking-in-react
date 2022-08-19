65c5ee586bd2507c5eef87c58401d548ab327c21
Walison Abram <abramwhas@gmail.com>
2022-08-19 14:56:24 -0300

    iteration 1 and 2 parcial

---
+++ src/components/ProductsTable.js
@@ -0,0 +1,22 @@
+function ProductsTable() {
+    return(
+        <div className="Table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ProductsTable;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+function SearchBar() {
+    return(
+        <div className='SearchBar'>
+            <p>Search</p>
+            <input className='SearchInput'></input><br />
+            <input type='checkbox' id='Checkbox'></input>
+            <label htmlFor="Checkbox"> Only show products in stock</label>
+        </div>
+    )
+}
+
+export default SearchBar;
