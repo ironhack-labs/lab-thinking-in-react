6a2b6118b7223229859072cbaef64713669cdea7
WilsonF78 <fortes.wilson78@gmail.com>
2022-09-03 15:47:57 +0200

    It----

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,1 +1,15 @@
+import { useState } from 'react';
+import jsonData from '../data.json';
+
+function ProductRow(props) {
+  const [productRow, setProductRow] = useState(jsonData);
+
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.price}</td>
+    </tr>
+  );
+}
+
 export default ProductRow;
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import jsonData from '../data.json';
+import ProductRow from './ProductRow';
 
 function ProductTable() {
   const [productTable, setProductTable] = useState(jsonData);
 
   return (
-    <div>
-      <h1>ProductTable</h1>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((el) => {
+          return <ProductRow name={el.name} price={el.price} />;
+        })}
+      </tbody>
+    </table>
   );
 }
 
