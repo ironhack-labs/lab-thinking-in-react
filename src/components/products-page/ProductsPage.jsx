017d8e53b71af8454dea736f836c44bb607a4aad
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-09-03 12:17:35 +0200

    feat: iterations 5 & 6 done

--- src/components/products-page/ProductsPage.jsx
+++ src/components/products-page/ProductsPage.jsx
@@ -4,13 +4,30 @@ import { useState } from 'react';
 import jsonData from './../../data.json';
 
 function ProductsPage() {
-  const [products, setProducts] = useState(jsonData);
+  const [stringSearch, setStringSearch] = useState('');
+  const [checkProduct, setCheckProduct] = useState(false);
+
+  const handleSearch = (event) => {
+    setStringSearch(event.target.value.toLowerCase());
+  };
+
+  const handleCheck = (event) => {
+    setCheckProduct(event.target.checked);
+  };
 
   return (
     <div className="container">
       <h1 className="text-center mt-3">IronStore</h1>
-      <SearchBar list={jsonData} />
-      <ProductTable products={jsonData} />
+      <SearchBar search={handleSearch} check={handleCheck} />
+      <ProductTable
+        products={jsonData
+          .filter((product) =>
+            stringSearch !== ''
+              ? product.name.toLowerCase().includes(stringSearch.toLowerCase())
+              : true
+          )
+          .filter((product) => (checkProduct ? product.inStock : true))}
+      />
     </div>
   );
 }
--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,18 +1,21 @@
-function SearchBar() {
+function SearchBar({search, check}) {
+
+
   return (
     <div className="container d-flex">
       <div className="input-group flex-column mt-3">
         <div className="form-outline">
           <h6 className="text-center">Search</h6>
-          <input type="search" name="search-bar" className="form-control" />
+          <input type="search" name="search-bar" className="form-control" onChange={search} />
         </div>
         <div className="my-3 form-check align-self-center">
           <input
             type="checkbox"
             className="form-check-input"
             id="exampleCheck1"
+            onChange={check}
           />
-          <label className="form-check-label" for="exampleCheck1">
+          <label className="form-check-label" for="checkProduct">
             Only show products in stock
           </label>
         </div>
