67648c3b17017091de89989f3b1390dac02f6134
WilsonF78 <fortes.wilson78@gmail.com>
2022-09-03 15:17:45 +0200

    Iteration 2 ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ProductsPages from './components/ProductsPages ';
+import SearchBar from './components/SearchBar';
+import ProductTable from './components/ProductTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductsPages></ProductsPages>
+      <SearchBar></SearchBar>
+      <ProductTable></ProductTable>
+      {/* <ProductRow></ProductRow> */}
     </div>
   );
 }
