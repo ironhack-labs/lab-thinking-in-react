05e27c4441ef92f3c5e2e67149f4150c41aadf85
WilsonF78 <fortes.wilson78@gmail.com>
2022-09-03 15:22:03 +0200

    Iteration 2 Ok v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import ProductsPages from './components/ProductsPages ';
-import SearchBar from './components/SearchBar';
-import ProductTable from './components/ProductTable';
 
 function App() {
   return (
