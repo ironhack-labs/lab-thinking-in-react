cb711db5e67a706272c93ae5591b14abd2cabf24
CrisArranz <c.arranz.arevalo@gmail.com>
2022-09-03 11:36:57 +0200

    Refactoring code

--- src/components/products-page/ProductPage.js
+++ src/components/products-page/ProductPage.js
@@ -6,31 +6,25 @@ import { useState } from 'react';
 import jsonData from '../../data.json';
 
 function ProductPage(){
-  const [products, setProducts] = useState(jsonData);
+  const [stringSearch, setStringSearch] = useState('');
+  const [checked, setChecked] = useState(false);
 
   const handleSearch = (event) => {
-    const inStockCheck = document.getElementById('checkInput').checked;
-    setProducts(
-      jsonData
-        .filter(product => inStockCheck ? product.inStock : true)
-        .filter(product => product.name.toLowerCase().includes(event.target.value.toLowerCase()))
-    )
+    setStringSearch(event.target.value.toLowerCase())
   }
 
   const handleCheck = (event) => {
-    const searchValue = document.getElementById('searchInput').value;
-    setProducts(
-      jsonData
-        .filter(product => event.target.checked ? product.inStock : true)
-        .filter(product => searchValue !== '' ? product.name.toLowerCase().includes(searchValue.toLowerCase()) : true)
-    )
+    setChecked(event.target.checked)
   }
 
   return (
     <div className="product-page container d-flex flex-column justify-content-center text-center mt-4">
       <h2>IronStore</h2>
       <SearchBar search={handleSearch} checked={handleCheck}/>
-      <ProductTable randomData={products}/>
+      <ProductTable randomData={
+        jsonData
+          .filter(product => checked ? product.inStock : true)
+          .filter(product => stringSearch !== '' ? product.name.toLowerCase().includes(stringSearch.toLowerCase()) : true)}/>
     </div>
   );
 }
