f32322e7b414b891091e73ab15050ab3919a1c56
RedValerian <r.pino.camacho@gmail.com>
2022-09-22 15:24:55 +0200

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ProductsPage from './pages/ProductPage';
 
 function App() {
   return (
     <div className="App">
+      <ProductsPage />
     </div>
   );
 }
 
 export default App;
+
