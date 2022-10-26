df89835d21697ac76188abcd6d41e91d05c088e9
Jandson Costa <jandsoncosta55@gmail.com>
2022-10-26 17:40:24 -0300

    save

---
+++ src/components/product/product.jsx
@@ -0,0 +1,11 @@
+function Product ({ price, inStock, name}){
+    return (
+      <>
+        <tr>
+          <td style={{ color: !inStock ? 'red' : 'black'}}>{ name }</td>
+          <td>{ price }</td>
+        </tr>
+      </>
+    )
+  }
+  export default Product;
