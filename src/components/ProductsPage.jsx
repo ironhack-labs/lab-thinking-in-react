73ffe4b737b503f5e0394dd429e8380c1b920f39
hvkst <bernd@hvkst.de>
2022-11-02 20:16:44 +0100

    refactoring round 3 - I guess that´s it ;D

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -4,7 +4,6 @@ import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
 function ProductsPage() {
-  // const [products, setProducts] = useState(jsonData); // Kann theoretisch weg
   const [checked, setChecked] = useState(false);
   const [searchInput, setSearchInput] = useState('');
 
@@ -14,56 +13,28 @@ function ProductsPage() {
 
   const inStockSwitch = (event) => {
     setChecked(event.target.checked);
-    // sortProducts();
   };
 
   // eslint-disable-next-line array-callback-return
-  let products = jsonData.filter((product) => {
-    if (checked) {
-      if (searchInput === '') {
-        return product.inStock === true;
-      } else if (
-        product.name.toLowerCase().includes(searchInput.toLowerCase())
-      ) {
+  const products = jsonData.filter((product) => {
+    if (searchInput === '') {
+      if (checked) {
         return product.inStock === true;
       }
-    } else if (!checked) {
-      if (searchInput === '') {
-        return product;
-      } else if (
-        product.name.toLowerCase().includes(searchInput.toLowerCase())
-      ) {
+      return product;
+    } else if (product.name.toLowerCase().includes(searchInput.toLowerCase())) {
+      if (checked) {
         return product.inStock === true;
       }
+      return product;
     }
   });
 
   return (
     <div>
-      <SearchBar {...{ checked, inStockSwitch, handleSearch, searchInput }} />
-      <ProductTable {...{ products, searchInput, inStockSwitch }} />
+      <SearchBar {...{ inStockSwitch, handleSearch, searchInput }} />
+      <ProductTable {...{ products }} />
     </div>
   );
 }
 export default ProductsPage;
-
-// refactoring round 1
-
-// eslint-disable-next-line array-callback-return
-// let products = jsonData.filter((product) => {
-//   if (checked && searchInput === '') {
-//     return product.inStock === true;
-//   } else if (!checked && searchInput === '') {
-//     return product;
-//   } else if (
-//     checked &&
-//     product.name.toLowerCase().includes(searchInput.toLowerCase())
-//   ) {
-//     return product.inStock === true;
-//   } else if (
-//     !checked &&
-//     product.name.toLowerCase().includes(searchInput.toLowerCase())
-//   ) {
-//     return product;
-//   }
-// });
