ebc50991085262465e54c00c1840fb50b9566106
sijincodes <sijinmathews03@gmail.com>
2022-11-30 21:51:00 +0100

    iteration2

---
+++ src/components/ProductTable.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProductTable = () => {
+  return (
+    <div>ProductTable</div>
+  )
+}
---
+++ src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
