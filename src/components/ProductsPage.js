c0ce7bb4e36c4faf418af7a566968f0218852939
sijincodes <sijinmathews03@gmail.com>
2022-11-30 22:47:57 +0100

    iteration4

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -7,12 +7,16 @@ import { ProductTable } from './ProductTable';
 
 export function ProductsPage () {
   const [products, setProducts] = useState(jsonData);
+  const [query, setQuery] = useState('');
+  const filteredProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(query.toLowerCase());
+  });
  
   return(
       <div>
         <h1>IronStore</h1>
-        <SearchBar />
-        <ProductTable products = {products}/>
+        <SearchBar setQuery={setQuery}/>
+        <ProductTable products = {filteredProducts}/>
       </div>    
   )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export const SearchBar = () => {
+export const SearchBar = ({setQuery}) => {
+    const handleSearchChange = event => {
+		setQuery(event.target.value)
+	}
   return (
-    <div>SearchBar</div>
+    <>
+        <input type="text" onChange={handleSearchChange}/>
+    </>
   )
 }
