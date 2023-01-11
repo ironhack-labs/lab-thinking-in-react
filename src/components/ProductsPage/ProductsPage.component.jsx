5dc5cbdb2a9235652e574a8bfdc5975fc5b80da1
Anil Ozsoy <anilozsoy@gmail.com>
2023-01-11 09:49:59 +0100

    Iteration 4 | Filter the Products

--- src/components/ProductsPage/ProductsPage.component.jsx
+++ src/components/ProductsPage/ProductsPage.component.jsx
@@ -6,11 +6,19 @@ import ProductTable from '../ProductTable/ProductTable.components';
 export default function ProductsPage() {
   //State
   const [products, setProducts] = useState(jsonData);
+  const [searchText, setSearchText] = useState('');
+
+  function handleSearchChange(searchText) {
+    const filteredProducts = products.filter((product) => {
+      return product.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setProducts(filteredProducts);
+  }
 
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar />
+      <SearchBar handleSearchChange={handleSearchChange} />
       <ProductTable products={products} />
     </div>
   );
--- src/components/SearchBar/SearchBar.component.jsx
+++ src/components/SearchBar/SearchBar.component.jsx
@@ -1,7 +1,14 @@
-export default function SearchBar() {
+export default function SearchBar(props) {
+  const { handleSearchChange } = props;
   return (
     <div>
-      <input type="search" />
+      <input
+        type="search"
+        placeholder="Search"
+        onChange={(event) => {
+          handleSearchChange(event.target.value);
+        }}
+      />
       <input type="checkbox" name="productsInStock" id="" value={false} />
       <label htmlFor="productsInStock">Only show products in stock</label>
     </div>
