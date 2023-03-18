1bd8bbf58508ad0c55fbce8752ce354d7fa15fa3
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-03-18 17:19:52 +0100

    Iteration 2

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,8 +1,12 @@
-import './productsPage.css';
+import { useState } from 'react';
 import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
+import jsonData from '../data.json';
+import './productsPage.css';
 
 const ProductsPage = () => {
+  const [products, setProducts] = useState(jsonData);
+
   return (
     <div>
       <SearchBar />
