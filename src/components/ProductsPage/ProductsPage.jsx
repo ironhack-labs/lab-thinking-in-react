930041d94cc8a3b1f3ca9b6ca42d4f5e1045166c
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.home>
2023-03-23 12:56:32 +0100

    iterations 1 and 2

---
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import jsonData from './../../data.json';
+
+
+function ProductsPage() {
+
+  const [products, setProducts] = useState(jsonData);
+
+    return (
+      <div>
+        <h1>IronStore</h1>
+      </div>   
+    )
+  }
+
+export default ProductsPage
