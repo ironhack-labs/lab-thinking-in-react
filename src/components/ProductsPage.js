f4dfcc8dea077006a2d960d1fc832739144e98f2
ficapp2.0 <ficapp2.0@gmail.com>
2023-03-27 22:01:28 +0200

    feat: iteration 5

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,23 +1,45 @@
-import React from 'react'
-import { useState } from 'react'
-import jsonData from './../data.json'
-import ProductTable from './ProductTable'
-import SearchBar from './SearchBar'
+import React from 'react';
+import { useState } from 'react';
+import jsonData from './../data.json';
+import ProductTable from './ProductTable';
+import SearchBar from './SearchBar';
+
+let currentSearch;
+let currentCheck;
 
 function ProductsPage() {
-  const [products, setProducts] = useState(jsonData)
+  const [products, setProducts] = useState(jsonData);
   const onChangeSearch = (text) => {
     setProducts((prevProducts) => {
-      return jsonData.filter(product => product.name.indexOf(text) !== -1)
-    })
-  }
+      currentSearch = text
+      return jsonData
+        .filter((product) => product.name.indexOf(text) !== -1)
+        .filter((product) =>
+          currentCheck
+            ? product.inStock === currentCheck
+            : product.inStock !== undefined
+        );
+    });
+  };
+  const onChangeCheck = (check) => {
+    currentCheck = check;
+    setProducts((prevProducts) =>
+      jsonData
+        .filter((product) => (check ? product.inStock === check : product))
+        .filter((product) => currentSearch ? product.name.indexOf(currentSearch) !== -1 : product)
+    );
+  };
+
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar onChangeSearch={onChangeSearch}/>
-      <ProductTable products={products}/>
+      <SearchBar
+        onChangeSearch={onChangeSearch}
+        onChangeCheck={onChangeCheck}
+      />
+      <ProductTable products={products} />
     </div>
-  )
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function SearchBar({ onChangeSearch }) {
-  const [search, setSearch] = useState('')
+function SearchBar({ onChangeSearch, onChangeCheck }) {
+  const [search, setSearch] = useState('');
   const handleChange = (event) => {
-    setSearch((prevSearch) => event.target.value)
-    onChangeSearch(event.target.value)
+    setSearch((prevSearch) => event.target.value);
+    onChangeSearch(event.target.value);
+  };
+
+  const [check, setCheck] = useState(false)
+  const handleCheck = (event) => {
+    setCheck((prevCheck) => event.target.checked)
+    onChangeCheck(event.target.checked)
   }
 
   return (
-    <input type="text" placeholder='Search...' value={search} onChange={handleChange}/>
-  )
+    <>
+      <div className="d-flex flex-column m-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={handleChange}
+          className="mb-3"
+        />
+        <div className="input-group d-flex justify-content-center">
+          <input type="checkbox" id="ProductsInStock" value={check} onChange={handleCheck}/>
+          <label className="checkbox-inline ms-2" htmlFor='ProductsInStock'>
+            Only show products in stock
+          </label>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
