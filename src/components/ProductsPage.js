502d7d54f27e99cefbf21c5a8926680a26db1b88
Sofía J <sofiajimglez@gmail.com>
2023-03-27 16:51:49 +0200

    Iteration 5 completed - filters by stock

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -7,17 +7,24 @@ import ProductTable from './ProductTable';
 export default function ProductsPage() {
   const [products, setProducts] = useState(jsonData);
   const [search, setSearch] = useState('');
+  const [seeStock, setSeeStock] = useState(false);
 
   const onSearch = (value) => {
     setSearch(value);
   }
 
-  const filteredProducts = products.filter(elem => elem.name.toLowerCase().includes(search.toLowerCase()));
+  const onSeeStock = () => {
+    setSeeStock(!seeStock);
+  }
+
+  const filteredProducts = products
+    .filter(elem => elem.name.toLowerCase().includes(search.toLowerCase()))
+    .filter(elem => seeStock === false || elem.inStock);
 
   return (
     <div className='d-flex flex-column align-items-center'>
       <h1 className='py-3 text-center'>IronStore</h1>
-      <SearchBar search={search} onSearch={onSearch} />
+      <SearchBar search={search} onSearch={onSearch} seeStock={seeStock} onSeeStock={onSeeStock} />
       <ProductTable products={filteredProducts} />
     </div>
   )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-export default function SearchBar({ search, onSearch }) {
+export default function SearchBar({ search, onSearch, seeStock, onSeeStock }) {
   const handleChange = (event) => {
     onSearch(event.target.value);
+  }
+
+  const handleSeeStock = (event) => {
+    onSeeStock(event.target.value);
   }
 
   return (
@@ -10,7 +14,7 @@ export default function SearchBar({ search, onSearch }) {
 
       <input type="text" className="form-control" id="search-bar" placeholder="Search product..." value={search} onChange={handleChange}/>
       <div className="form-check py-3">
-        <input className="form-check-input" type="checkbox" value="" id="in-stock" />
+        <input className="form-check-input" type="checkbox" value={seeStock} id="in-stock" onChange={handleSeeStock} />
         <label className="form-check-label" htmlFor="in-stock">Only show products in stock</label>
       </div>
     </div>
