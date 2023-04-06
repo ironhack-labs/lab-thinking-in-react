c66e35d687f25cb215b718f96ed3bee43bf492ed
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-04-06 09:42:09 +0200

    solved lab

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -6,27 +6,39 @@ import ProductTable from './ProductTable';
 function ProductsPage () {
   const [products, setProducts] = useState(jsonData);
   const [keyword, setKeyword] = useState("")
+  const [isInStock, setisInStock] = useState(false)
 
-  const keywordChange = (e) => {
+  const handleKeyword = (e) => {
     setKeyword(e.target.value)
   }
 
-  const results = products.filter(product => 
+  const handleCheckbox = (e) => {
+    setisInStock(e.target.checked)
+  }
+
+  const keywordResults = products.filter(product => 
     product.name.toLowerCase().includes(keyword.toLowerCase())
+  )
+
+  const stockResults = keywordResults.filter(product => 
+    isInStock ? product.inStock : true
   )
 
   return(
       <div>
         <h1>IronStore</h1>
         
-        <SearchBar keyword={keyword} onChange={keywordChange} />
+        <SearchBar keyword={keyword} onChange={handleKeyword} />
         <div className="checkbox">
-          <input type="checkbox" className="form-check-input" id="flexCheckDefault" />
+          <input type="checkbox" className="form-check-input" id="flexCheckDefault" 
+            checked={products.inStock} 
+            onChange={handleCheckbox}
+          />
           <label className="form-check-label" for="flexCheckDefault">
             Only show products in stock
           </label>
         </div>
-        <ProductTable products={results}/>
+        <ProductTable products={stockResults}/>
 
       </div>    
   )
