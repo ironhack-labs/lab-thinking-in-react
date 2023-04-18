4ac77a857ac929f2a89641cf155b8649d38804fe
michael07200 <mickmhtong@gmail.com>
2023-04-18 18:55:02 +0200

    build: Iteration 1

---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductRow = () => {
+  return (
+    <div>ProductRow</div>
+  )
+}
+
+export default ProductRow
---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+    <div>ProductsPage</div>
+  )
+}
+
+export default ProductsPage
