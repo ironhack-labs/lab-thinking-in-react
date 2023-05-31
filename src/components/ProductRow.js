6c3ff313390d33873089a9e6b5afe0f1db9d5874
Natassa <112552395+natko22@users.noreply.github.com>
2023-05-31 10:16:16 +0200

    finished iteration 3

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,14 +1,15 @@
-
-
-function ProductRow({oneProduct}) {
+function ProductRow({ oneProduct }) {
+  const coloredProducts = {
+    color: oneProduct.inStock ? 'black' : 'red',
+  };
   return (
     <>
-      <tr>
+      <tr style={coloredProducts}>
         <td>{oneProduct.name}</td>
         <td>{oneProduct.price}</td>
       </tr>
     </>
-  )
+  );
 }
 
-export default ProductRow
+export default ProductRow;
