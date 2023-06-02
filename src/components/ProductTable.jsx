2d8adfb2b8e3ba39c2568c8f5d6aee797ef2344d
IreneValor <115938535+IreneValor@users.noreply.github.com>
2023-06-02 22:00:20 +0200

    inicio table sin products

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,1 +1,28 @@
-import React from "react";
+import React from 'react';
+
+const ProductTable = ({ products }) => {
+  return (
+    <div  className='ProductTable'>
+      <table>
+        <thead>
+         <tr>      
+            <th>Nombre</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
+
+
