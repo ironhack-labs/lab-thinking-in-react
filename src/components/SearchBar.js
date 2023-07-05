d04c49b35fab2f18479cc2ad5d7a0ece0ae372f9
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-05 11:03:15 -0400

    LAB COMPLETE

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function SearchBar( {searchInput, setSearchInput} ) {
-// SearchBar recieves {searchInput, setSearchInput} as props
+export default function SearchBar( {searchInput, setSearchInput, showInStock, setShowInStock} ) {
+// SearchBar recieves {searchInput, setSearchInput, showInStock, setShowInStock} as props
 
   const handleSearchBar = (event) => {
     // Extract the  value of the input field using event.target.value and store it in the newSearch variable.
@@ -9,7 +9,13 @@ export default function SearchBar( {searchInput, setSearchInput} ) {
     // Update the searchInput state using
     setSearchInput(newSearch);
     // This will trigger a re-render of the ProductPage component and update the searchInput value.
+  }
 
+  const handleCheckBox = (event) => {
+    // Extract the 'checked' property from the event target to determine whether the checkbox is checked or not.
+const checked = event.target.checked;
+// Update the showInStock state
+setShowInStock(checked);
   }
 
   return (
@@ -18,7 +24,7 @@ export default function SearchBar( {searchInput, setSearchInput} ) {
     <input className="search-input" onChange={handleSearchBar}/>
     {/* When the user types somtheing, OnChange event is triggered and calls the function */}
     <label>
-      <input type="checkbox" />
+      <input type="checkbox" checked={showInStock} onChange={handleCheckBox} />
       Only show products in stock
     </label>
     
