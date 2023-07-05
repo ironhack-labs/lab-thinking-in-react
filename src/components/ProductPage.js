3d2a0db05a1d8be217d06deba86206cd5b9eb2b3
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-05 09:42:26 -0400

    Iteration #4 COMPLETE

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,10 +6,21 @@ import ProductTable from './ProductTable';
 export default function ProductPage() {
   const [products, setProducts] = useState(() => productData);
 
+  // Search input state to keep trach of the search input entered by the user
+  const [searchInput, setSearchInput] = useState(() => '');
+  // Filter product 
+  const filteredProducts = products.filter(product => {
+    return product.name.toLowerCase().includes(searchInput.toLowerCase());
+  })
+  // It filters the products array based on the search input ==>
+  // The filtered products are then mapped and rendered using the ProductTable component ==>
+  // Each filtered product is passed as a prop to the ProductTable component ==>
+  // The ProductTable component receives the product prop and renders a ProductRow component for each product.
+
   return (
     <div className='big-container'>
       <h1>IronStore</h1>
-      <SearchBar />
+      <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
 
       <table>
       <thead className='table-heading'>
@@ -20,7 +31,7 @@ export default function ProductPage() {
     </thead>
 
     <tbody>
-    {products.map((product) => <ProductTable key={product.id} product={product} />)}
+    {filteredProducts.map((product) => <ProductTable key={product.id} product={product} />)}
     </tbody>
       </table>
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar( {searchInput, setSearchInput} ) {
+// SearchBar recieves {searchInput, setSearchInput} as props
+
+  const handleSearchBar = (event) => {
+    // Extract the  value of the input field using event.target.value and store it in the newSearch variable.
+    const newSearch = event.target.value;
+    // Update the searchInput state using
+    setSearchInput(newSearch);
+    // This will trigger a re-render of the ProductPage component and update the searchInput value.
+
+  }
 
   return (
     <div className="searchBar-container">
     <h3>Search</h3>
-    <input className="search-input" />
+    <input className="search-input" onChange={handleSearchBar}/>
+    {/* When the user types somtheing, OnChange event is triggered and calls the function */}
     <label>
       <input type="checkbox" />
       Only show products in stock
