cd91bc211ee4c62de4451101a83e0e44ae6f5314
jonp001 <jonperez01@outlook.com>
2023-07-06 22:50:20 -0400

    completed iterations 1-2

---
+++ src/components/ProductTable.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ProductTable() {
+  return (
+    <div className='productTable'>
+    <table>
+      <thead>
+        <tr>
+            <th>Name</th>
+            <th>Price </th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+    
+    </div>
+  )
+}
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SearchBar() {
+  return (
+    <div className='searchBar'>
+
+<label for = "search-site"> Search This Site:
+<input type="search" id="search-site" name="search">    
+</input>
+</label>
+
+    </div>
+  )
+}
