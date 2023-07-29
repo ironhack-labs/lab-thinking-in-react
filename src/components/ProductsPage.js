444672319b7194143c87347ea30c17a009be0f8e
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-07-29 13:25:27 +0100

    Iteration 4

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -6,13 +6,27 @@ import ProductTable from './ProductTable';
 function ProductsPage () {
   const [products, setProducts] = useState(jsonData);
 
+  function handleSearch(e) {
+
+    const productsCopy = [...jsonData]
+
+    const filterBySearch = productsCopy.filter((product) => {
+        if (product.name.toLowerCase().includes(e)) { return product; }
+
+    })
+
+    setProducts(filterBySearch)
+
+  }
+
   return(
       <div>
         <h1>IronStore</h1>
-        <SearchBar />
-        <ProductTable products={products} />
+        <SearchBar  handleSearch={handleSearch}/>
+        <ProductTable products={products}  />
       </div>    
-  )
+  );
 }
 
-export default ProductsPage
+
+export default ProductsPage;
