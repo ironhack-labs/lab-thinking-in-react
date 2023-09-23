aaa94f3ef659822f55f5ed73f4c9187a5073aeeb
Bright Mensah <brightmensah064@gmail.com>
2023-09-23 11:04:34 +0000

    thinking react

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function SearchBar({ handleFilter }) {
+  const [query, setQuery] = useState('');
+  return (
+    <div className="search-bar">
+      <label>Search</label>
+      <input
+        value={query}
+        type="text"
+        onChange={(e) => {
+          handleFilter(e.target.value);
+          setQuery(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
