b05bd0b03b73846ccfccd93963395f5c8ebb60ee
VeronicaGL <veronicagalvez88@gmail.com>
2023-10-09 15:45:31 +0200

    lab ironStore

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -2,13 +2,14 @@ function ProductRow ({ product }) {
   const {name, price, inStock} = product;
   const changeColor = inStock ? "grey" : "red";
   
-
   return (
     <>
       <tr>
       <th style={{ color: changeColor, fontSize: '14px', fontWeight: 'normal' }}>{name}</th>
       <th style={{ color: changeColor, fontSize: '14px', fontWeight: 'normal' }}>{price}</th>
       </tr>
+
+      
     </>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,32 +2,60 @@ import React, { useState } from 'react';
 
 function SearchBar({ products }) {
   const [search, setSearch] = useState('');
+  const [showOnlyInStock, setShowOnlyInStock] = useState('');
   
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
 
-  const filteredProducts = products.filter((products) => {
-    if (products.name.toUpperCase().includes(search.toUpperCase())) {
-      return true;
-    }
+  const handleCheckboxChange = (e) => {
+    setShowOnlyInStock(e.target.checked);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.name.toUpperCase().includes(search.toUpperCase());
+    const inStock = product.inStock;
     
+    if (showOnlyInStock) {
+      return matchesSearch && inStock;
+    } else {
+      return matchesSearch;
+    }
   });
 
   return (
-    <div className="mt-4">
+    <>
+      <div className="mt-4">
 
-      <input
-        type="text"
-        placeholder="Search product..."
-        value={search}
-        onChange={handleChange}
-      />
-        {filteredProducts.map((products) => (
-          <li key={products.id}>{products.name}</li>
-        ))}
-      
-    </div>
+        <input
+          type="text"
+          placeholder="Search product..."
+          value={search}
+          onChange={handleChange}
+        />
+          {filteredProducts.length > 0 ? (
+            search && filteredProducts.map((product) => (
+           <p key={product.id}>{product.name}</p>
+           ))
+          ) : (
+           <p>No products in stock</p>
+          )}
+        
+      </div>
+
+      <div className="form-check form-check-inline mt-5">
+        <input 
+          className="form-check-input" 
+          type="checkbox" 
+          checked={showOnlyInStock} 
+          onChange={handleCheckboxChange} 
+          id="flexCheckDefault"
+        />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+          Only show products in stock
+        </label>
+      </div>
+    </>
   );
 }
 
