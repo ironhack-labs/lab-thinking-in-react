98acd9ec9563cb3dcf768cfaa9974c8597c7a655
Eduardo Botelho <eduardo11224b@gmail.com>
2024-01-25 13:49:38 +0000

    done, feat: Implement the filtering of the products

--- src/components/Products/SearchBar/index.jsx
+++ src/components/Products/SearchBar/index.jsx
@@ -1,13 +1,31 @@
 import './SearchBar.css';
 
-export default function SearchBar() {
+export default function SearchBar({ setFilter }) {
+  function updateNameFilter(e) {
+    setFilter((filter) => ({ ...filter, productName: e.target.value }));
+  }
+
+  function updateOnlyStockFilter(e) {
+    setFilter((filter) => ({ ...filter, onlyStock: e.target.checked }));
+  }
+
   return (
     <section id="search" className="SearchBar">
       <form id="search-form">
         <span>Search</span>
-        <input id="product-name" name="product-name" type="text" />
+        <input
+          onChange={updateNameFilter}
+          id="product-name"
+          name="product-name"
+          type="text"
+        />
         <fieldset className="checkbox-fieldset">
-          <input type="checkbox" name="only-stock" id="only-stock" />
+          <input
+            onChange={updateOnlyStockFilter}
+            type="checkbox"
+            name="only-stock"
+            id="only-stock"
+          />
           <label htmlFor="only-stock">Only show products in stock</label>
         </fieldset>
       </form>
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -5,14 +5,23 @@ import SearchBar from '../../components/Products/SearchBar';
 import ProductTable from '../../components/Products/ProductTable';
 
 export default function Products() {
-  const [products, setProducts] = useState(jsonData);
+  const [filter, setFilter] = useState({
+    productName: '',
+    onlyStock: false,
+  });
+
+  const filteredProducts = jsonData.filter((product) => {
+    const validName = product.name.startsWith(filter.productName);
+    const validStockValue = !filter.onlyStock || product.inStock;
+    return validName && validStockValue;
+  });
 
   return (
     <div id="products-page">
       <h1 className="page-title">IronStore</h1>
       <div className="wrapper">
-        <SearchBar />
-        <ProductTable products={products} />
+        <SearchBar setFilter={setFilter} />
+        <ProductTable products={filteredProducts} />
       </div>
     </div>
   );
