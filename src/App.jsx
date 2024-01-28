056278197be5129919469350a37134bf7f46cf76
Lorena Barrera <l.barreranicolas@gmail.com>
2024-01-28 14:18:50 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import ProductsPage from "./Components/ProductsPage/ProductsPage"
 
 function App() {
   return (
     <div className="App">
+
       <h1>React | Thinking in React</h1>
+      <ProductsPage />
     </div>
   );
 }
 
-export default App;
+export default App
