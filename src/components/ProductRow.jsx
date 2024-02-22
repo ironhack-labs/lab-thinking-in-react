47ae71ac239e10fb9533fc9fd6913fd5bd3ceeae
alberto-sclocchi <scloks75tv@gmail.com>
2024-02-22 00:29:59 -0500

    lab done

---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProductRow(product) {
+  
+  return (
+    <tr>
+        <td style={{color: !product.inStock && 'red'}}>{product.name}</td>
+        <td>{product.price}</td>
+    </tr>
+  )
+}
---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import jsonData from "../data.json";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+
+export default function ProductsPage() {
+
+  const [products, setProducts] = useState(jsonData);
+
+  const searchedProducts = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.value.length);
+
+    const userInput = event.target.value.toLowerCase();
+
+    if(userInput.length > 0){
+      const searchedProduct = products.filter((product) => {
+        const lowerCaseProduct = product.name.toLowerCase();
+        return lowerCaseProduct.includes(userInput);
+      });
+      
+      setProducts(searchedProduct);
+    }
+    else{
+      setProducts(jsonData);
+    }
+  }
+
+  const inStockProducts = (event) => {
+    if (event.target.checked){
+      const inStockList = products.filter((product) => product.inStock)
+      setProducts(inStockList);
+    }
+    else {
+        setProducts(jsonData);
+    }
+  }
+
+  return (
+    <div>
+      <h1>IronStore</h1>
+      <SearchBar inStockFeature={inStockProducts} searchFeature={searchedProducts}/>
+      <ProductTable productsList={products}/>
+    </div>
+  );
+}
+
