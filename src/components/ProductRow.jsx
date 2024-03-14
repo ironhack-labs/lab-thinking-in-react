2ad2547e89915190eaf83eee12ba2f7fc19ed4c7
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-03-14 17:06:33 +0100

    add iteration 3

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import products from '../data.json'
 
 
 function ProductRow({product}) {
   return (
-    <div>{product.name}</div>
+    <div>
+    <table className="table">
+    <tr>
+      <th> Name</th>
+      <th>Price</th>
+    </tr >
+    <tbody>
+      {products.map((product) => 
+      <tr key={product.id} product={product}>
+        <td>{product.name}</td>
+        <td>{product.price}</td>
+      </tr>
+      )}
+      
+    </tbody>
+    </table>
+    </div>
   )
 }
 
