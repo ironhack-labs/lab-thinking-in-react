c8094ce135425e58ca47be79fb6e7309e1159b54
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-03-15 22:48:12 +0100

    iteration 3 corrected

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,27 +1,14 @@
 import React from 'react'
-import products from '../data.json'
 
 
 function ProductRow({product}) {
   return (
-    <div>
-    <table className="table">
     <tr>
-      <th> Name</th>
-      <th>Price</th>
-    </tr >
-    <tbody>
-      {products.map((product) => 
-      <tr key={product.id} product={product}>
-        <td>{product.name}</td>
+        <td style={{color: product.inStock ? "black" : "red"}}>{product.name}</td>
         <td>{product.price}</td>
       </tr>
-      )}
-      
-    </tbody>
-    </table>
-    </div>
   )
 }
 
 export default ProductRow
+
--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -4,9 +4,19 @@ import ProductRow from './ProductRow'
 
 function ProductTable({products}) {
   return (
-    <div>{products.map((product)=>{
-        return <ProductRow key={product.id} product={product}/>
-    })}</div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <ProductRow key={product.id} product={product} />
+        ))}
+      </tbody>
+    </table>
   )
 }
 
